5472ea422512d68ed7b0884df32d7a0efb4298e3
Kyaw Thu <evanch98@gmail.com>
2023-07-16 00:01:54 +0800

    update the Main component

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -3,8 +3,22 @@
 const Main = () => {
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center">
-      <div className="max-w-[1240px] flex items-center justify-center">
-        Main
+      <div className="w-full px-2 lg:px-0 lg:max-w-[1240px] flex items-center justify-between">
+        <section
+          className="lg:w-[350px] h-full"
+          aria-label="Introduction section"
+        >
+          <h1
+            className="text-2xl text-center lg:text-left lg:text-4xl font-bold lg:leading-[60px]"
+            aria-label="Introduction Title"
+          >
+            HEY! I'm Kyaw Thu, AN ASPIRING FRONT-END DEVELOPER BASED IN TAIWAN
+          </h1>
+          <p>
+            I am currently pursuing a Bachelor of Science in Computer Science at
+            the University of the People.
+          </p>
+        </section>
       </div>
     </div>
   );
