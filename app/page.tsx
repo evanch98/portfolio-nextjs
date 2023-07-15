eb1ee98721baf8448259557585fc1a759d65dbe2
Kyaw Thu <evanch98@gmail.com>
2023-07-15 23:33:26 +0800

    add the Main component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
+import Main from "@/components/main/Main";
 import Image from "next/image";
 
 export default function Home() {
-  return <main>Main</main>;
+  return (
+    <main>
+      <Main />
+    </main>
+  );
 }
