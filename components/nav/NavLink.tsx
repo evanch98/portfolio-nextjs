c7ba58e37d24d3b5d178391a0d75b41114021d6a
Kyaw Thu <evanch98@gmail.com>
2023-07-15 21:52:44 +0800

    update the NavLink component

---
+++ components/nav/NavLink.tsx
@@ -0,0 +1,15 @@
+interface NavLinkProps {
+  href: string;
+  name: string;
+  isActive?: boolean;
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ href, name, isActive }) => {
+  return (
+    <a href={href}>
+      <p>{name}</p>
+    </a>
+  );
+};
+
+export default NavLink;
