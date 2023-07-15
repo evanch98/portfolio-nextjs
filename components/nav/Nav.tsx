980da89b8a7daa8309706a0b77befb32dc013932
Kyaw Thu <evanch98@gmail.com>
2023-07-15 21:57:51 +0800

    update the Nav component

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,24 +1,39 @@
 "use client";
 
+import { useState } from "react";
 import Logo from "./Logo";
 import NavLink from "./NavLink";
 
 const Nav = () => {
+  const [active, setActive] = useState("home");
+  const pathname: string = window.location.pathname;
   return (
     <nav className="w-full px-5 lg:px-10 shadow-xl flex justify-between items-center">
       <Logo />
-      <ul className="hidden lg:flex justify-between space-x-10">
-        <li>
-          <NavLink href="/" name="Home" />
+      <ul className="hidden lg:flex justify-between items-end space-x-10">
+        <li onClick={() => setActive("home")}>
+          <NavLink href="/" name="Home" isActive={pathname === "/"} />
         </li>
-        <li>
-          <NavLink href="/projects" name="projects" />
+        <li onClick={() => setActive("projects")}>
+          <NavLink
+            href="/projects"
+            name="Projects"
+            isActive={pathname.includes("/projects")}
+          />
         </li>
-        <li>
-          <NavLink href="/about" name="About" />
+        <li onClick={() => setActive("about")}>
+          <NavLink
+            href="/about"
+            name="About"
+            isActive={pathname === "/about"}
+          />
         </li>
-        <li>
-          <NavLink href="/blog" name="Blog" />
+        <li onClick={() => setActive("blog")}>
+          <NavLink
+            href="/blog"
+            name="Blog"
+            isActive={pathname.includes("/blog")}
+          />
         </li>
       </ul>
       <button className="hidden lg:flex btn btn-primary">Contact Me</button>
