99436a28eb5b58a291b033ccce272c655803a825
Kyaw Thu <evanch98@gmail.com>
2023-07-15 23:44:29 +0800

    update the Main component

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -1,7 +1,13 @@
 "use client";
 
 const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center">
+      <div className="max-w-[1240px] flex items-center justify-center">
+        Main
+      </div>
+    </div>
+  );
 };
 
 export default Main;
