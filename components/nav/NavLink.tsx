ca1f42fc4314f1aa44396260c6a1f81a3c17aa4c
Kyaw Thu <evanch98@gmail.com>
2023-07-15 21:57:15 +0800

    update the NavLink component

--- components/nav/NavLink.tsx
+++ components/nav/NavLink.tsx
@@ -1,14 +1,37 @@
 "use client"
 
+import { motion } from "framer-motion";
+
 interface NavLinkProps {
   href: string;
   name: string;
-  isActive?: boolean;
+  isActive: boolean;
 }
+
+const circleVariants = {
+  hidden: {
+    y: "-50vh",
+  },
+  visible: {
+    y: 0,
+    transition: {
+      type: "spring",
+      stiffness: "120",
+    },
+  },
+};
 
 const NavLink: React.FC<NavLinkProps> = ({ href, name, isActive }) => {
   return (
-    <a href={href}>
+    <a href={href} className="flex flex-col items-center justify-center">
+      {isActive && (
+        <motion.div
+          className="w-2 h-2 rounded-full bg-[#8C6196]"
+          variants={circleVariants}
+          initial="hidden"
+          animate="visible"
+        ></motion.div>
+      )}
       <p>{name}</p>
     </a>
   );
