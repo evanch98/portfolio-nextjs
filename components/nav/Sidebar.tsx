584b88837e9c4988a45973371c6e6411f81f76f7
Kyaw Thu <evanch98@gmail.com>
2023-07-20 16:04:37 +0800

    update the Sidebar component

--- components/nav/Sidebar.tsx
+++ components/nav/Sidebar.tsx
@@ -1,5 +1,35 @@
+import useSidebar from "@/hooks/useSidebar";
+import Logo from "./Logo";
+import { AiOutlineClose } from "react-icons/ai";
+import { motion } from "framer-motion";
+
+const sidebarVariants = {
+  hidden: { x: "150vw" },
+  visible: {
+    x: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const sidebar = useSidebar();
+  return (
+    <motion.aside
+      className="absolute top-0 right-0 w-full h-screen lg:hidden bg-[#fcd968] z-50"
+      variants={sidebarVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <div className="flex flex-col p-5">
+        <section className="flex items-center justify-between">
+          <Logo />
+          <AiOutlineClose className="w-9 h-auto" onClick={sidebar.onClose} />
+        </section>
+      </div>
+    </motion.aside>
+  );
 };
 
 export default Sidebar;
