45c8d1f28be668a5bf1be5d39874efb2554c91c4
Kyaw Thu <evanch98@gmail.com>
2023-07-20 15:50:55 +0800

    update the Nav component

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -10,37 +10,42 @@ const Nav = () => {
   const pathname = usePathname();
   const sidebar = useSidebar();
   return (
-    <nav className="w-full px-5 lg:px-10 py-2 shadow-xl flex justify-between items-center fixed top-0 left-0">
-      <Logo />
-      <ul className="hidden lg:flex justify-between items-end space-x-10">
-        <li>
-          <NavLink href="/" name="Home" isActive={pathname === "/"} />
-        </li>
-        <li>
-          <NavLink
-            href="/projects"
-            name="Projects"
-            isActive={pathname.includes("/projects")}
-          />
-        </li>
-        <li>
-          <NavLink
-            href="/about"
-            name="About"
-            isActive={pathname.includes("/about")}
-          />
-        </li>
-        <li>
-          <NavLink
-            href="/blog"
-            name="Blog"
-            isActive={pathname.includes("/blog")}
-          />
-        </li>
-      </ul>
-      <button className="hidden lg:flex btn btn-primary">Contact Me</button>
-      <AiOutlineMenu className="lg:hidden w-9 h-auto" onClick={sidebar.onOpen} />
-    </nav>
+    <>
+      <nav className="w-full px-5 lg:px-10 py-2 shadow-xl flex justify-between items-center fixed top-0 left-0">
+        <Logo />
+        <ul className="hidden lg:flex justify-between items-end space-x-10">
+          <li>
+            <NavLink href="/" name="Home" isActive={pathname === "/"} />
+          </li>
+          <li>
+            <NavLink
+              href="/projects"
+              name="Projects"
+              isActive={pathname.includes("/projects")}
+            />
+          </li>
+          <li>
+            <NavLink
+              href="/about"
+              name="About"
+              isActive={pathname.includes("/about")}
+            />
+          </li>
+          <li>
+            <NavLink
+              href="/blog"
+              name="Blog"
+              isActive={pathname.includes("/blog")}
+            />
+          </li>
+        </ul>
+        <button className="hidden lg:flex btn btn-primary">Contact Me</button>
+        <AiOutlineMenu
+          className="lg:hidden w-9 h-auto"
+          onClick={sidebar.onOpen}
+        />
+      </nav>
+    </>
   );
 };
 
