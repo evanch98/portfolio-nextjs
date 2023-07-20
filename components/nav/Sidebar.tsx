3d2d6b131fd8ea3d96193816e1799cd326c06879
Kyaw Thu <evanch98@gmail.com>
2023-07-20 16:38:25 +0800

    update the Sidebar component

--- components/nav/Sidebar.tsx
+++ components/nav/Sidebar.tsx
@@ -3,6 +3,7 @@ import Logo from "./Logo";
 import { AiOutlineClose } from "react-icons/ai";
 import { motion } from "framer-motion";
 import NavLink from "./NavLink";
+import { usePathname } from "next/navigation";
 
 const sidebarVariants = {
   hidden: { x: "150vw", transition: { duration: 0.5 } },
@@ -16,21 +17,67 @@ const sidebarVariants = {
 
 const Sidebar = () => {
   const sidebar = useSidebar();
+  const pathname = usePathname();
   return (
     <motion.aside
       className="absolute top-0 right-0 w-full h-screen lg:hidden bg-[#fcd968] z-50"
+      aria-label="Sidebar"
       variants={sidebarVariants}
       initial="hidden"
       animate="visible"
       exit="hidden"
     >
-      <div className="flex flex-col p-5">
-        <section className="flex items-center justify-between">
+      <div className="flex flex-col p-5" aria-label="Sidebar Contents">
+        <section
+          className="flex items-center justify-between"
+          aria-label="Logo and Close Button Section"
+        >
           <Logo />
-          <AiOutlineClose className="w-9 h-auto" onClick={sidebar.onClose} />
+          <AiOutlineClose
+            className="w-9 h-auto"
+            onClick={sidebar.onClose}
+            aria-label="Close the Sidebar"
+          />
         </section>
-        <section className="flex flex-col mt-5">
-          <NavLink href="/" name="Home" isActive={true} />
+        <section
+          className="flex flex-col mt-5 gap-5"
+          aria-label="Navigation Link Section"
+        >
+          <NavLink
+            href="/"
+            name="Home"
+            isActive={pathname === "/"}
+            onClick={sidebar.onClose}
+            className="text-2xl"
+          />
+          <NavLink
+            href="/projects"
+            name="Project"
+            isActive={pathname.includes("/projects")}
+            onClick={sidebar.onClose}
+            className="text-2xl"
+          />
+          <NavLink
+            href="/about"
+            name="About"
+            isActive={pathname.includes("/about")}
+            onClick={sidebar.onClose}
+            className="text-2xl"
+          />
+          <NavLink
+            href="/blog"
+            name="Blog"
+            isActive={pathname.includes("/blog")}
+            onClick={sidebar.onClose}
+            className="text-2xl"
+          />
+          <NavLink
+            href="/contact"
+            name="Contact Me"
+            isActive={pathname.includes("/contact")}
+            onClick={sidebar.onClose}
+            className="text-2xl"
+          />
         </section>
       </div>
     </motion.aside>
