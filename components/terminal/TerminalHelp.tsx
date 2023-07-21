c2cd01d971157eacad0b78bb6effbf5565d5889c
Kyaw Thu <evanch98@gmail.com>
2023-07-21 19:39:29 +0800

    update the Terminal component

--- components/terminal/TerminalHelp.tsx
+++ components/terminal/TerminalHelp.tsx
@@ -2,11 +2,23 @@
 
 import useTerminalHelp from "@/hooks/useTerminalHelp";
 import { FiHelpCircle, FiXCircle } from "react-icons/fi";
+import { motion } from "framer-motion";
+
+const variants = {
+  hidden: { x: 200, transition: { duration: 0.5 } },
+  visible: { x: 0, transition: { duration: 0.5 } },
+};
 
 const TerminalHelp = () => {
   const terminalHelp = useTerminalHelp();
   return (
-    <div className="absolute top-0 right-0 bg-gradient-to-b from-[#D9D9D9] to-[#9A9A9A] h-full w-2/3 lg:w-1/2 p-2">
+    <motion.div
+      className="absolute top-0 right-0 bg-gradient-to-b from-[#D9D9D9] to-[#9A9A9A] h-full w-2/3 lg:w-1/2 p-2"
+      variants={variants}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+    >
       <div className="flex items-center justify-between">
         <div className="flex items-center justify-center gap-1">
           <h1 className="font-bold">Help</h1>
@@ -14,7 +26,7 @@ const TerminalHelp = () => {
         </div>
         <FiXCircle onClick={terminalHelp.onClose} />
       </div>
-    </div>
+    </motion.div>
   );
 };
 
