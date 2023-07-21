9487104fc1ca0dc40024bad5332eee0327ae609f
Kyaw Thu <evanch98@gmail.com>
2023-07-21 18:47:37 +0800

    update the Body component

--- components/terminal/Body.tsx
+++ components/terminal/Body.tsx
@@ -5,7 +5,7 @@ import Nav from "./Nav";
 
 const Body = () => {
   return (
-    <div className="w-[550px] h-[320px] bg-black rounded-lg overflow-hidden terminal-shadow">
+    <div className="w-full lg:w-[550px] h-[320px] bg-black rounded-lg overflow-hidden terminal-shadow">
       <Nav />
       <div className="w-full p-2 terminal text-sm text-white">
         <p>Last login: {timeProvider()}</p>
