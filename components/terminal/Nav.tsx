223dd2346e278e0e80fbcb2a3ab13e0b34b41ead
Kyaw Thu <evanch98@gmail.com>
2023-07-21 17:36:20 +0800

    update the Nav component

--- components/terminal/Nav.tsx
+++ components/terminal/Nav.tsx
@@ -4,9 +4,13 @@ import Button from "./Button";
 
 const Nav = () => {
   return (
-    <div className="w-[550px] h-6 bg-gradient-to-b from-[#D9D9D9] to-[#9A9A9A] flex items-center justify-center">
-      <Button />
-      <p className="text-sm">kyawthu</p>
+    <div className="w-[550px] h-6 bg-gradient-to-b from-[#D9D9D9] to-[#9A9A9A] flex items-center justify-start px-2">
+      <div className="flex gap-1">
+        <Button className="bg-red-600" />
+        <Button className="bg-yellow-600" />
+        <Button className="bg-green-600" />
+      </div>
+      <p className="text-sm w-full text-center">kyawthu</p>
     </div>
   );
 };
