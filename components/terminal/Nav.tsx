f704d8c5365de321baa5265360321fd6efd425c3
Kyaw Thu <evanch98@gmail.com>
2023-07-21 17:28:22 +0800

    add the Button component

--- components/terminal/Nav.tsx
+++ components/terminal/Nav.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import Button from "./Button";
+
 const Nav = () => {
   return (
     <div className="w-[550px] h-6 bg-gradient-to-b from-[#D9D9D9] to-[#9A9A9A] flex items-center justify-center">
+      <Button />
       <p className="text-sm">kyawthu</p>
     </div>
   );
