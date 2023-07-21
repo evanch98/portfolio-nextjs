566c825f3d8e6150be6f683895716d6aeca98d5e
Kyaw Thu <evanch98@gmail.com>
2023-07-21 17:30:57 +0800

    update the Button component

--- components/terminal/Button.tsx
+++ components/terminal/Button.tsx
@@ -1,7 +1,13 @@
 "use client";
 
-const Button = () => {
-  return <div>Button</div>;
+import { twMerge } from "tailwind-merge";
+
+interface ButtonProps {
+  className: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ className }) => {
+  return <div className={twMerge("w-3 h-3 rounded-ful", className)}></div>;
 };
 
 export default Button;
