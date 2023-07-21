702f266b1152177c71c999f4fae7b6bedb56c628
Kyaw Thu <evanch98@gmail.com>
2023-07-21 17:39:22 +0800

    update the terminal/Nav component

--- components/terminal/Nav.tsx
+++ components/terminal/Nav.tsx
@@ -1,16 +1,18 @@
 "use client";
 
 import Button from "./Button";
+import { FiHelpCircle } from "react-icons/fi";
 
 const Nav = () => {
   return (
-    <div className="w-[550px] h-6 bg-gradient-to-b from-[#D9D9D9] to-[#9A9A9A] flex items-center justify-start px-2">
+    <div className="w-[550px] h-6 bg-gradient-to-b from-[#D9D9D9] to-[#9A9A9A] flex items-center justify-between px-2">
       <div className="flex gap-1">
         <Button className="bg-red-600" />
         <Button className="bg-yellow-600" />
         <Button className="bg-green-600" />
       </div>
-      <p className="text-sm w-full text-center">kyawthu</p>
+      <p className="text-sm">kyawthu</p>
+      <FiHelpCircle />
     </div>
   );
 };
