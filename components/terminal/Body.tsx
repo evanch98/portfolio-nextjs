43b82a6e1a0724ffad129a3a1681df5368c0d4bf
Kyaw Thu <evanch98@gmail.com>
2023-07-21 19:11:00 +0800

    update the Body component

--- components/terminal/Body.tsx
+++ components/terminal/Body.tsx
@@ -1,17 +1,29 @@
 "use client";
 
+import { motion } from "framer-motion";
 import { timeProvider } from "@/utils/dateProvider";
 import Nav from "./Nav";
 
+const variants = {
+  hidden: { scale: 0, transition: { duration: 1 } },
+  visible: { scale: 1, transition: { duration: 1 } },
+};
+
 const Body = () => {
   return (
-    <div className="w-full lg:w-[550px] h-[320px] bg-black rounded-lg overflow-hidden terminal-shadow">
+    <motion.div
+      className="w-full lg:w-[550px] h-[320px] bg-black rounded-lg overflow-hidden terminal-shadow"
+      variants={variants}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+    >
       <Nav />
       <div className="w-full p-2 terminal text-sm text-white">
         <p>Last login: {timeProvider()}</p>
         <p>kyawthu ~ %</p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
