4c718d0aee9b087415a479ba098c662b429bf687
Kyaw Thu <evanch98@gmail.com>
2023-07-21 18:24:00 +0800

    update the page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import ClientOnly from "@/components/common/ClientOnly";
 import Main from "@/components/main/Main";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <Main />
-    </main>
+    <ClientOnly>
+      <main>
+        <Main />
+      </main>
+    </ClientOnly>
   );
 }
