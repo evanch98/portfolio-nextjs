187563e6267b39d59a3d9903abf55dbca04392ed
Kyaw Thu <evanch98@gmail.com>
2023-07-21 17:43:55 +0800

    update the terminal/Nav component

--- components/terminal/Nav.tsx
+++ components/terminal/Nav.tsx
@@ -5,7 +5,7 @@ import { FiHelpCircle } from "react-icons/fi";
 
 const Nav = () => {
   return (
-    <div className="w-[550px] h-6 bg-gradient-to-b from-[#D9D9D9] to-[#9A9A9A] flex items-center justify-between px-2 fixed top-0 left-0">
+    <div className="w-[550px] h-6 bg-gradient-to-b from-[#D9D9D9] to-[#9A9A9A] flex items-center justify-between px-2">
       <div className="flex gap-1">
         <Button className="bg-red-600" />
         <Button className="bg-yellow-600" />
