a802751996c1ec6378a2afd9709ba75f5eaedbf8
Kyaw Thu <evanch98@gmail.com>
2023-07-21 19:58:21 +0800

    update the TerminalHelp component

--- components/terminal/TerminalHelp.tsx
+++ components/terminal/TerminalHelp.tsx
@@ -13,7 +13,7 @@ const TerminalHelp = () => {
   const terminalHelp = useTerminalHelp();
   return (
     <motion.div
-      className="absolute top-0 right-0 bg-gradient-to-b from-[#D9D9D9] to-[#9A9A9A] h-full w-2/3 lg:w-1/2 p-2"
+      className="absolute top-0 right-0 bg-gradient-to-b from-[#D9D9D9] to-[#9A9A9A] h-full w-2/3 lg:w-1/2 p-2 text-black"
       variants={variants}
       initial="hidden"
       animate="visible"
@@ -26,6 +26,16 @@ const TerminalHelp = () => {
         </div>
         <FiXCircle onClick={terminalHelp.onClose} />
       </div>
+      {/* TODO: Divider */}
+      <div className="flex flex-col text-sm">
+        <h2 className="font-semibold text-lg">Commands</h2>
+        <p>ls: List all the available pages</p>
+        <p>cd {"<page name>"}: Go to the page</p>
+        <p>clear: Clear</p>
+      </div>
+      <p className="text-xs text-center text-red-600 absolute bottom-1">
+        This is not an actual terminal. Only the above commands will work.
+      </p>
     </motion.div>
   );
 };
