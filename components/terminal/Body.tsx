27ddb543e336fea05ba928cdf2d68b0e0eab2703
Kyaw Thu <evanch98@gmail.com>
2023-07-21 17:54:41 +0800

    update the body component

--- components/terminal/Body.tsx
+++ components/terminal/Body.tsx
@@ -6,6 +6,10 @@ const Body = () => {
   return (
     <div className="w-[550px] h-[320px] bg-black rounded-lg overflow-hidden">
       <Nav />
+      <div className="w-full p-2 terminal text-sm text-white">
+        <p>Last login: Tue Jan 18 14:49:46</p>
+        <p>kyawthu ~ %</p>
+      </div>
     </div>
   );
 };
