8af0416b4d286e72c2c3cf31a5d6b8fab4354706
Kyaw Thu <evanch98@gmail.com>
2023-07-21 17:26:47 +0800

    update the terminal/Nav component

--- components/terminal/Nav.tsx
+++ components/terminal/Nav.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <div className="w-[550px] h-6 bg-gradient-to-b from-[#D9D9D9] to-[#9A9A9A] flex items-center justify-center">
+      <p className="text-sm">kyawthu</p>
+    </div>
+  );
 };
 
 export default Nav;
