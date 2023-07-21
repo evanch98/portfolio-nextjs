342fd0f4f6b20a56961ec0359b5acc6f858836e6
Kyaw Thu <evanch98@gmail.com>
2023-07-21 17:22:59 +0800

    add the Nav component

--- components/terminal/Terminal.tsx
+++ components/terminal/Terminal.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import Nav from "./Nav";
+
 const Terminal = () => {
-  return <div>Terminal</div>;
+  return (
+    <div>
+      <Nav />
+    </div>
+  );
 };
 
 export default Terminal;
