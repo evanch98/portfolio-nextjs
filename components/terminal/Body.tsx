0150f9a7a145ee3fb7ae8cd2d0a38bf27c3cc7d4
Kyaw Thu <evanch98@gmail.com>
2023-07-21 18:05:03 +0800

    update the Body component

--- components/terminal/Body.tsx
+++ components/terminal/Body.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { timeProvider } from "@/utils/dateProvider";
 import Nav from "./Nav";
 
 const Body = () => {
@@ -7,7 +8,7 @@ const Body = () => {
     <div className="w-[550px] h-[320px] bg-black rounded-lg overflow-hidden terminal-shadow">
       <Nav />
       <div className="w-full p-2 terminal text-sm text-white">
-        <p>Last login: Tue Jan 18 14:49:46</p>
+        <p>Last login: {timeProvider()}</p>
         <p>kyawthu ~ %</p>
       </div>
     </div>
