4c8f584ef92862be5c15c880438b4f7ed8c54003
Kyaw Thu <evanch98@gmail.com>
2023-07-21 19:19:30 +0800

    update the Body component

--- components/terminal/TerminalHelp.tsx
+++ components/terminal/TerminalHelp.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 const TerminalHelp = () => {
-  return <div className="absolute top-0 right-0">Terminal Help</div>;
+  return <div className="absolute top-0 right-0 bg-D9D9D9">Terminal Help</div>;
 };
 
 export default TerminalHelp;
