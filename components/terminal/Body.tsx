5885e62567992e614bba1e13173ba6527a763c6a
Kyaw Thu <evanch98@gmail.com>
2023-07-21 17:41:05 +0800

    update the Body component

--- components/terminal/Body.tsx
+++ components/terminal/Body.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import Nav from "./Nav";
+
 const Body = () => {
-  return <div>Body</div>;
+  return (
+    <div className="relative w-[550px] h-[320px] bg-black">
+      <Nav />
+    </div>
+  );
 };
 
 export default Body;
