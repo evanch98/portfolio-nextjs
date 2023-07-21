b8a7f7017631c80dbcb0be7ac57ab1e8d6611c52
Kyaw Thu <evanch98@gmail.com>
2023-07-21 18:17:42 +0800

    update the dateProvider.ts

--- utils/dateProvider.ts
+++ utils/dateProvider.ts
@@ -1,33 +1,60 @@
+const dayConverter = (day: number) => {
+  switch (day) {
+    case 0:
+      return "Sun";
+    case 1:
+      return "Mon";
+    case 2:
+      return "Tue";
+    case 3:
+      return "Wed";
+    case 4:
+      return "Thu";
+    case 5:
+      return "Fri";
+    case 6:
+      return "Sat";
+    default:
+      return "";
+  }
+};
+
+const monthConverter = (month: number) => {
+  switch (month) {
+    case 0:
+      return "Jan";
+    case 1:
+      return "Feb";
+    case 2:
+      return "Mar";
+    case 3:
+      return "Apr";
+    case 4:
+      return "May";
+    case 5:
+      return "Jun";
+    case 6:
+      return "Jul";
+    case 7:
+      return "Aug";
+    case 8:
+      return "Sep";
+    case 9:
+      return "Oct";
+    case 10:
+      return "Nov";
+    case 11:
+      return "Dec";
+    default:
+      return "";
+  }
+};
+
 export const timeProvider = () => {
   const date = new Date();
-  let day: string = "";
-  switch (date.getDay()) {
-    case 0:
-      day = "Sun";
-      break;
-    case 1:
-      day = "Mon";
-      break;
-    case 2:
-      day = "Tue";
-      break;
-    case 3:
-      day = "Wed";
-      break;
-    case 4:
-      day = "Thu";
-      break;
-    case 5:
-      day = "Fri";
-      break;
-    case 6:
-      day = " Sat";
-      break;
-    default:
-      day = "";
-      break;
-  }
-  return `${day} ${date.getMonth()} ${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const day: string = dayConverter(date.getDay());
+  const month: string = monthConverter(date.getMonth());
+  return `${day} ${month} ${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 };
 
 export const fullYearProvider = () => {
