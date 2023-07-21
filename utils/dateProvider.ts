fbd5999577c469908846b735b939a9901a4b1211
Kyaw Thu <evanch98@gmail.com>
2023-07-21 18:08:37 +0800

    update the dateProvider.ts

--- utils/dateProvider.ts
+++ utils/dateProvider.ts
@@ -1,6 +1,33 @@
 export const timeProvider = () => {
   const date = new Date();
-  return `${date.getDay()} ${date.getMonth()} ${date.getDate()} ${date.getHours()} ${date.getMinutes()} ${date.getSeconds()}`;
+  let day: string = "";
+  switch (date.getDay()) {
+    case 0:
+      day = "Sun";
+      break;
+    case 1:
+      day = "Mon";
+      break;
+    case 2:
+      day = "Tue";
+      break;
+    case 3:
+      day = "Wed";
+      break;
+    case 4:
+      day = "Thu";
+      break;
+    case 5:
+      day = "Fri";
+      break;
+    case 6:
+      day = " Sat";
+      break;
+    default:
+      day = "";
+      break;
+  }
+  return `${day} ${date.getMonth()} ${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
 };
 
 export const fullYearProvider = () => {
