7d2615fcd565f4c9776de68deb94ab79f1ff7567
Kyaw Thu <evanch98@gmail.com>
2023-07-21 17:20:46 +0800

    update the Main component

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import Terminal from "../terminal/Terminal";
 
 const text: string =
-  "HEY! I'm Kyaw Thu, An Aspiring Front-end developer based in Taiwan";
+  "HEY!+ I'm+ Kyaw+ Thu,+ An+ Aspiring+ Front-end+ developer+ based+ in+ Taiwan+";
 
 const Main = () => {
   return (
@@ -17,17 +17,16 @@ const Main = () => {
         aria-label="Main Section"
       >
         <section
-          className="w-full lg:max-w-[450px] h-full"
+          className="lg:max-w-[380px] h-full"
           aria-label="Introduction section"
         >
           <h1
-            className="hidden lg:text-left lg:text-4xl font-bold lg:leading-[60px] lg:flex lg:flex-auto uppercase cursor-default"
+            className="text-2xl text-center lg:text-left lg:text-4xl font-bold lg:leading-[60px] uppercase cursor-default"
             aria-label="Introduction Title"
           >
-            {text.split(" ").map((text, index) => (
+            {text.split("+").map((text, index) => (
               <motion.span
                 key={index}
-                className="mr-2"
                 whileHover={{
                   color: "#8C6196",
                   transition: {
@@ -39,12 +38,6 @@ const Main = () => {
                 {text}
               </motion.span>
             ))}
-          </h1>
-          <h1
-            className="lg:hidden text-2xl text-center uppercase font-bold"
-            aria-label="Introduction Title"
-          >
-            HEY! I'm Kyaw Thu, An Aspiring Front-end developer based in Taiwan
           </h1>
           <p
             className="opacity-70 text-center lg:text-left mt-5"
