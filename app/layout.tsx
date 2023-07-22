ce5199cc1a2c83df1c940d7d03351f46d736c25d
Kyaw Thu <evanch98@gmail.com>
2023-07-22 21:59:15 +0800

    update the layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import TerminalProvider from "@/components/providers/TerminalProvider";
+import FloatingButton from "@/components/floating-button/FloatingButton";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body className={`${inter.className} relative`}>
         <Nav />
         <TerminalProvider />
+        <FloatingButton />
         {children}
       </body>
     </html>
