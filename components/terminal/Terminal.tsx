fc394987464cdbb6dcb3cac71819c3e6ab908bc0
Kyaw Thu <evanch98@gmail.com>
2023-07-22 22:11:55 +0800

    update the Terminal component

--- components/terminal/Terminal.tsx
+++ components/terminal/Terminal.tsx
@@ -4,7 +4,7 @@ import Body from "./Body";
 
 const Terminal = () => {
   return (
-    <div className="w-full h-full flex justify-center items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="w-full h-full flex justify-center items-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-40">
       <Body />
     </div>
   );
