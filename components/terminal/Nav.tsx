816f38e1ebf47faf9b06ddadef00f454f55135d4
Kyaw Thu <evanch98@gmail.com>
2023-07-22 22:28:51 +0800

    update the nav/Nav component

--- components/terminal/Nav.tsx
+++ components/terminal/Nav.tsx
@@ -12,8 +12,8 @@ const Nav = () => {
     <div className="w-full h-6 bg-gradient-to-b from-[#D9D9D9] to-[#9A9A9A] flex items-center justify-between px-2">
       <div className="flex gap-1">
         <Button
-          className="bg-red-600 cursor-pointer flex items-center justify-center"
-          children={<FiX />}
+          className="bg-red-600 cursor-pointer flex flex-col items-center justify-center"
+          children={<FiX className="w-2 h-2" />}
           onclick={terminal.onClose}
         />
         <Button className="bg-yellow-600" />
