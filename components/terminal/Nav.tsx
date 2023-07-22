9473870ae069e14ca5b9147bcd87ab524b0c2548
Kyaw Thu <evanch98@gmail.com>
2023-07-22 22:23:54 +0800

    update the terminal/Nav component

--- components/terminal/Nav.tsx
+++ components/terminal/Nav.tsx
@@ -2,14 +2,20 @@
 
 import useTerminalHelp from "@/hooks/useTerminalHelp";
 import Button from "./Button";
-import { FiHelpCircle } from "react-icons/fi";
+import { FiHelpCircle, FiX } from "react-icons/fi";
+import useTerminal from "@/hooks/useTerminal";
 
 const Nav = () => {
   const terminalHelp = useTerminalHelp();
+  const terminal = useTerminal();
   return (
     <div className="w-full h-6 bg-gradient-to-b from-[#D9D9D9] to-[#9A9A9A] flex items-center justify-between px-2">
       <div className="flex gap-1">
-        <Button className="bg-red-600" />
+        <Button
+          className="bg-red-600 cursor-pointer flex items-center justify-center"
+          children={<FiX />}
+          onclick={terminal.onClose}
+        />
         <Button className="bg-yellow-600" />
         <Button className="bg-green-600" />
       </div>
