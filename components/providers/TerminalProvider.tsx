6f59fb7c59a82848ece47e93351209bbb5b829f1
Kyaw Thu <evanch98@gmail.com>
2023-07-22 21:24:17 +0800

    update the TerminalProvider component

--- components/providers/TerminalProvider.tsx
+++ components/providers/TerminalProvider.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import useTerminal from "@/hooks/useTerminal";
 import Terminal from "../terminal/Terminal";
 
 const TerminalProvider = () => {
-  return <Terminal />;
+  const terminal = useTerminal();
+  return terminal.isOpen && <Terminal />;
 };
 
 export default TerminalProvider;
