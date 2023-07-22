14f73e53c100e75a2b20e3a4cc417883696fa532
Kyaw Thu <evanch98@gmail.com>
2023-07-22 22:03:44 +0800

    update the FloatingButton.tsx

--- components/floating-button/FloatingButton.tsx
+++ components/floating-button/FloatingButton.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { FiTool } from "react-icons/fi";
+
 const FloatingButton = () => {
-  return <div className="fixed bottom-5 right-5">FloatingButton</div>;
+  return (
+    <button className="fixed bottom-5 right-5 btn btn-secondary">
+      <FiTool className="w-5 h-auto" />
+    </button>
+  );
 };
 
 export default FloatingButton;
