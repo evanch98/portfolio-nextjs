046f4f45df74445a98323d84efd7d3ab5287dad1
Kyaw Thu <evanch98@gmail.com>
2023-07-23 12:23:10 +0800

    update the FloatingButton component

--- components/floating-button/FloatingButton.tsx
+++ components/floating-button/FloatingButton.tsx
@@ -7,7 +7,7 @@ const FloatingButton = () => {
   const terminal = useTerminal();
   return (
     <button
-      className="fixed bottom-5 right-5 w-12 h-12 rounded-full btn-secondary flex items-center justify-center floating-button-shadow hover:animate-bounce"
+      className="fixed bottom-5 right-5 w-12 h-12 rounded-full btn-secondary flex items-center justify-center floating-button-shadow hover:animate-bounce z-50"
       onClick={terminal.onOpen}
     >
       <FiTool className="w-5 h-auto" />
