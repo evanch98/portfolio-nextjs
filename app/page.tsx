2e98a5243b54ce8735a9cb259d8e5bec4f6a4059
Kyaw Thu <evanch98@gmail.com>
2023-07-23 12:27:32 +0800

    update the page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import ClientOnly from "@/components/common/ClientOnly";
 import Main from "@/components/main/Main";
-import Image from "next/image";
+import RecentProjects from "@/components/recent-projects/RecentProjects";
 
 export default function Home() {
   return (
     <ClientOnly>
       <main>
         <Main />
+        <RecentProjects />
       </main>
     </ClientOnly>
   );
