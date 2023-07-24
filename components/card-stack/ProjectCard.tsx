5230dac4d9ca221eb783280acf24211979ac9977
Kyaw Thu <evanch98@gmail.com>
2023-07-25 00:29:17 +0800

    update the PRojectCard component

--- components/card-stack/ProjectCard.tsx
+++ components/card-stack/ProjectCard.tsx
@@ -1,7 +1,19 @@
 "use client";
 
-const ProjectCard = () => {
-  return <div>Project Card</div>;
+import { twMerge } from "tailwind-merge";
+
+interface ProjectCardProps {
+  className?: string;
+}
+
+const ProjectCard: React.FC<ProjectCardProps> = ({ className }) => {
+  return (
+    <div
+      className={twMerge("w-[200px] h-[250px] bg-black rounded-2xl", className)}
+    >
+      Project Card
+    </div>
+  );
 };
 
 export default ProjectCard;
