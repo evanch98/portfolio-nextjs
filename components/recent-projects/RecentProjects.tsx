49107cef5a8bc6d7babdf8961000e8bff1b50737
Kyaw Thu <evanch98@gmail.com>
2023-07-24 23:34:16 +0800

    update the RecentProjects component

--- components/recent-projects/RecentProjects.tsx
+++ components/recent-projects/RecentProjects.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 const RecentProjects = () => {
-  return <div className="w-full h-screen">Recent Projects</div>;
+  return <div id="recent-projects" className="w-full h-screen">Recent Projects</div>;
 };
 
 export default RecentProjects;
