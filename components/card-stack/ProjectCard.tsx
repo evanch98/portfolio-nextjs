3436344facab70ca6844e4a5ffc8aea54703ead6
Kyaw Thu <evanch98@gmail.com>
2023-07-25 23:39:28 +0800

    update the ProjectCard component

--- components/card-stack/ProjectCard.tsx
+++ components/card-stack/ProjectCard.tsx
@@ -1,19 +1,24 @@
 "use client";
 
 import { twMerge } from "tailwind-merge";
-import { motion } from "framer-motion";
+import { Variants, motion } from "framer-motion";
 
 interface ProjectCardProps {
   className?: string;
+  projectCardVariants?: Variants;
 }
 
-const ProjectCard: React.FC<ProjectCardProps> = ({ className }) => {
+const ProjectCard: React.FC<ProjectCardProps> = ({
+  className,
+  projectCardVariants,
+}) => {
   return (
     <motion.div
       className={twMerge(
         "w-[200px] h-[250px] bg-black rounded-2xl project-card-shadow",
         className
       )}
+      variants={projectCardVariants}
     >
       Project Card
     </motion.div>
