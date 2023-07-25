0a5d0b8f7c30b2485151e59af980147c8c48522c
Kyaw Thu <evanch98@gmail.com>
2023-07-26 01:09:14 +0800

    update the RecentProjects component

--- components/recent-projects/RecentProjects.tsx
+++ components/recent-projects/RecentProjects.tsx
@@ -11,7 +11,19 @@ const RecentProjects = () => {
       className="w-full h-screen flex flex-col justify-center items-center bg-white"
       aria-label="Recent Projects Section"
     >
-      <div></div>
+      <div className="outer-container">
+        <h1 className="text-3xl lg:text-4xl text-[#8C6196] font-bold">
+          Recent Projects
+        </h1>
+        <article className="mt-5 flex gap-5 w-full items-center justify-center">
+          <section className="w-full flex items-center justify-center">
+            <CardStack />
+          </section>
+          <section className="w-full flex items-center justify-center">
+            <CardStack />
+          </section>
+        </article>
+      </div>
     </Element>
   );
 };
