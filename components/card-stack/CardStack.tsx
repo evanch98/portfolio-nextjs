1d53fc5face67d038cc5beca23c18a4fd4f70348
Kyaw Thu <evanch98@gmail.com>
2023-07-26 00:46:59 +0800

    update the CardStack component

--- components/card-stack/CardStack.tsx
+++ components/card-stack/CardStack.tsx
@@ -1,14 +1,41 @@
 "use client";
 
 import ProjectCard from "./ProjectCard";
+import { motion } from "framer-motion";
+
+const firstCardVariants = {
+  hover: { rotate: -20 },
+};
+
+const secondCardVariants = {
+  hover: { y: -200 },
+};
+
+const thirdCardVariants = {
+  hover: { rotate: 20 },
+};
 
 const CardStack = () => {
   return (
-    <div className="relative w-[300px] h-[300px]">
-      <ProjectCard className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[#FCD968] z-50" />
-      <ProjectCard className="absolute top-[45.5%] left-[57%] -translate-x-1/2 -translate-y-1/2 bg-[#9FB1EF] rotate-[10deg] z-40" />
-      <ProjectCard className="absolute top-[42%] left-[64.5%] -translate-x-1/2 -translate-y-1/2 bg-[#8C6196] rotate-[20deg] z-30" />
-    </div>
+    <motion.div
+      initial="initial"
+      animate="animate"
+      whileHover="hover"
+      className="relative cursor-pointer"
+    >
+      <ProjectCard
+        projectCardVariants={firstCardVariants}
+        className="absolute top-0 left-0 bg-[#FCD968] z-50"
+      />
+      <ProjectCard
+        projectCardVariants={secondCardVariants}
+        className="absolute top-0 left-2 bg-[#9FB1EF] z-40"
+      />
+      <ProjectCard
+        projectCardVariants={thirdCardVariants}
+        className="absolute top-0 left-4 bg-[#8C6196] z-30"
+      />
+    </motion.div>
   );
 };
 
