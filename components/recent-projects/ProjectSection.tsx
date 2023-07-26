5cbe5d9783ca2fef61f429250d6d42b7709ea41a
Kyaw Thu <evanch98@gmail.com>
2023-07-26 17:56:42 +0800

    update the ProjectSection component

--- components/recent-projects/ProjectSection.tsx
+++ components/recent-projects/ProjectSection.tsx
@@ -4,20 +4,44 @@ import CardStack from "../card-stack/CardStack";
 
 interface ProjectSectionProps {
   cardSide: "left" | "right";
+  title: string;
+  description: string;
+  framework: string;
+  gitHubLink?: string;
+  demoLink?: string;
+  pageUrl?: string;
 }
 
-const ProjectSection: React.FC<ProjectSectionProps> = ({ cardSide }) => {
+const ProjectSection: React.FC<ProjectSectionProps> = ({
+  cardSide,
+  title,
+  description,
+  framework,
+  gitHubLink,
+  demoLink,
+  pageUrl,
+}) => {
   return (
     <div className="mt-5 flex gap-5 w-full items-center justify-center">
-      <section className="w-full flex items-center justify-center">
-        <CardStack
-          firstCardChildren={<div>Airbnb Clone</div>}
-          secondCardChildren={<div>GitHub</div>}
-        />
+      {cardSide === "left" && (
+        <section className="w-full flex items-center justify-center">
+          <CardStack
+            firstCardChildren={<div>Airbnb Clone</div>}
+            secondCardChildren={<div>GitHub</div>}
+          />
+        </section>
+      )}
+      <section className="w-full flex flex-col items-center justify-center">
+        <h2 className="font-bold text-2xl">{title}</h2>
       </section>
-      <section className="w-full flex items-center justify-center">
-        Airbnb Clone
-      </section>
+      {cardSide === "right" && (
+        <section className="w-full flex items-center justify-center">
+          <CardStack
+            firstCardChildren={<div>Airbnb Clone</div>}
+            secondCardChildren={<div>GitHub</div>}
+          />
+        </section>
+      )}
     </div>
   );
 };
