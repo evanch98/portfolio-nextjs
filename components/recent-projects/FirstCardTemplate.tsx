a440deb7db21d48b8e11c9ad3bd90f73f150bde1
Kyaw Thu <evanch98@gmail.com>
2023-07-26 18:36:38 +0800

    update the FirstCardTemplate component

--- components/recent-projects/FirstCardTemplate.tsx
+++ components/recent-projects/FirstCardTemplate.tsx
@@ -2,11 +2,22 @@
 
 import airbnb from "@/public/assets/projects/airbnb.svg";
 import Image from "next/image";
+import Link from "next/link";
 
 const FirstCardTemplate = () => {
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
-      <Image src={airbnb} alt="Airbnb" />
+    <div className="w-full h-full flex flex-col items-center justify-center relative group">
+      <Image
+        src={airbnb}
+        alt="Airbnb"
+        className="group-hover:opacity-0 transition"
+      />
+      <Link
+        href="/airbnb"
+        className="opacity-0 group-hover:opacity-100 hover:underline absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition"
+      >
+        Learn more
+      </Link>
     </div>
   );
 };
