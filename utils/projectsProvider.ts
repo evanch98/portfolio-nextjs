6660634f2bb2e7c0d79aa8a476ae4a4b24b150b9
Kyaw Thu <evanch98@gmail.com>
2023-07-26 19:26:28 +0800

    update the projectsProvider.ts

--- utils/projectsProvider.ts
+++ utils/projectsProvider.ts
@@ -1,7 +1,7 @@
+import { projectType } from "@/types/types";
 import { airbnbProject } from "./projects";
 
-const projectsProvider = {
-  airbnb: airbnbProject,
-};
+const projectsProvider = new Map<string, projectType>();
+projectsProvider.set("airbnb", airbnbProject);
 
 export default projectsProvider;
