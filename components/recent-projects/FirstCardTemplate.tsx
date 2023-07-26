bbc798eeff9b5f246f13b176bee0a7abd0504460
Kyaw Thu <evanch98@gmail.com>
2023-07-26 18:15:11 +0800

    update the FirstCardTemplate.tsx

--- components/recent-projects/FirstCardTemplate.tsx
+++ components/recent-projects/FirstCardTemplate.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 const FirstCardTemplate = () => {
-  return <div>First Card</div>;
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      First Card
+    </div>
+  );
 };
 
 export default FirstCardTemplate;
