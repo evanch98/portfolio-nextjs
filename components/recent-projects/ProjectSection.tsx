be30859af3eb82654eeff434d0415a237d40a4c7
Kyaw Thu <evanch98@gmail.com>
2023-07-26 17:48:20 +0800

    update the ProjectSection component

--- components/recent-projects/ProjectSection.tsx
+++ components/recent-projects/ProjectSection.tsx
@@ -1,7 +1,21 @@
 "use client";
 
+import CardStack from "../card-stack/CardStack";
+
 const ProjectSection = () => {
-  return <div>Project Section</div>;
+  return (
+    <div className="mt-5 flex gap-5 w-full items-center justify-center">
+      <section className="w-full flex items-center justify-center">
+        <CardStack
+          firstCardChildren={<div>Airbnb Clone</div>}
+          secondCardChildren={<div>GitHub</div>}
+        />
+      </section>
+      <section className="w-full flex items-center justify-center">
+        Airbnb Clone
+      </section>
+    </div>
+  );
 };
 
 export default ProjectSection;
