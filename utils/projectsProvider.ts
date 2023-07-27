39abcf4affd4aa8986caa83843b458def9062015
Kyaw Thu <evanch98@gmail.com>
2023-07-27 21:46:21 +0800

    update the projectsProvider.ts

--- utils/projectsProvider.ts
+++ utils/projectsProvider.ts
@@ -1,6 +1,11 @@
 import { projectType } from "@/types/types";
 import { airbnbProject, spotifyProject } from "./projects";
 
-const projectsProvider: projectType[] = [spotifyProject, airbnbProject];
+const projectsProvider: projectType[] = [
+  spotifyProject,
+  airbnbProject,
+  spotifyProject,
+  airbnbProject,
+];
 
 export default projectsProvider;
