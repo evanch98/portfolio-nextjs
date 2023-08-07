efe7c4455c959069661eb5285ea9e043d2a8052d
Kyaw Thu <evanch98@gmail.com>
2023-08-08 00:15:57 +0800

    update the Banner component

--- app/(root)/components/banner.tsx
+++ app/(root)/components/banner.tsx
@@ -5,7 +5,7 @@ import ResumeIcon from "@/components/common/icons/ResumeIcon";
 
 const Banner = () => {
   return (
-    <div className="w-full py-8 flex flex-col items-center justify-center bg-[#282B30] rounded-2xl gap-y-2">
+    <div className="w-full py-5 flex flex-col items-center justify-center bg-[#282B30] rounded-2xl gap-y-2">
       <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">Kyaw Thu</h1>
       <p className="opacity-70 md:text-lg lg:text-xl">An aspiring front-end developer</p>
       <div className="flex items-center justify-center gap-x-4">
