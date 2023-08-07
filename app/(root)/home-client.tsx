cfd1c93859d4866584c6706c073066b408af7f32
Kyaw Thu <evanch98@gmail.com>
2023-08-08 00:04:19 +0800

    update the HomeClient component

--- app/(root)/home-client.tsx
+++ app/(root)/home-client.tsx
@@ -5,7 +5,7 @@ import Banner from "./components/banner";
 const HomeClient = () => {
   return (
     <div className="w-full h-full mt-[64px] flex items-center justify-center">
-      <div className="h-full max-w-[1240px] flex flex-col items-center justify-center">
+      <div className="h-full w-full max-w-[1240px] flex flex-col items-center justify-center">
         <Banner />
       </div>
     </div>
