636576b8f84c6835cf6c0057160541dd429b221c
Kyaw Thu <evanch98@gmail.com>
2023-08-07 23:23:44 +0800

    update the NavLink component

--- components/nav-link.tsx
+++ components/nav-link.tsx
@@ -1,7 +1,19 @@
 "use client";
 
-const NavLink = () => {
-  return <div>NavLink</div>;
+import { IconType } from "react-icons";
+
+interface NavLinkProps {
+  label: string;
+  icon: IconType;
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ label, icon: Icon }) => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-y-2">
+      {label}
+      <Icon className="w-6 h-6" />
+    </div>
+  );
 };
 
 export default NavLink;
