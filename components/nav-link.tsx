3a7c4277570b5629be921945702b8263dac1dadb
Kyaw Thu <evanch98@gmail.com>
2023-08-07 23:31:40 +0800

    update the NavLink component

--- components/nav-link.tsx
+++ components/nav-link.tsx
@@ -10,7 +10,7 @@ interface NavLinkProps {
 const NavLink: React.FC<NavLinkProps> = ({ label, icon: Icon }) => {
   return (
     <div className="flex flex-col items-center justify-center gap-y-1 text-xs">
-      <Icon className="w-6 h-6" />
+      <Icon className="w-5 h-5" />
       {label}
     </div>
   );
