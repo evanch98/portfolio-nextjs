ba1c7029858fee046a9b59dd35a928e62fad9660
Kyaw Thu <evanch98@gmail.com>
2023-08-07 23:55:32 +0800

    update the HomeClient component

--- app/(root)/home-client.tsx
+++ app/(root)/home-client.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 const HomeClient = () => {
-  return <div>Home</div>;
+  return (
+    <div className="w-full h-full mt-[64px] flex items-center justify-center">
+      Home
+    </div>
+  );
 };
 
 export default HomeClient;
