aff814f38f90957039afba87ae51e37231dd9d79
Kyaw Thu <evanch98@gmail.com>
2023-08-08 16:15:11 +0800

    update the IntroSection component

--- app/(pages)/about/components/intro-section.tsx
+++ app/(pages)/about/components/intro-section.tsx
@@ -2,6 +2,12 @@ const IntroSection = () => {
   return (
     <div className="w-full p-5 rounded-2xl bg-[#282B30] flex flex-col gap-y-2">
       <p>
+        My name is Kyaw Thu, and a lot of people know me as Evan. I have born
+        and raised in Burma (Myanmar), and I am currently living in Taiwan. I am
+        pursuing my Bachelor of Science in Computer Science at the University of
+        the People.
+      </p>
+      <p>
         I have spent 7 years studying medicine at the University of Medicine 1,
         Yangon. Due to several reasons, including pandemic, I had to drop out of
         the university. For as long as I can remember, I have always been into
