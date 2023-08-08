cbbd0affd598bc0a6ccf4ba772c8e21b7ca9e3cf
Kyaw Thu <evanch98@gmail.com>
2023-08-08 15:28:27 +0800

    update the ToolsSection component

--- app/(root)/components/tools-section.tsx
+++ app/(root)/components/tools-section.tsx
@@ -1,5 +1,37 @@
+import Card from "@/components/common/card";
+
 const ToolsSection = () => {
-  return <div>Tools</div>;
+  const tools = [
+    {
+      title: "Next JS",
+      description:
+        "I have built several web applications, including Airbnb Clone, Spotify Clone, and Admin Dashboard, using Next JS framework.",
+      info: "https://nextjs.org/",
+    },
+    {
+      title: "React",
+      description: "I use React library for building website user interface.",
+      info: "https://react.dev/",
+    },
+    {
+      title: "Tailwind CSS",
+      description: "",
+      info: "",
+    },
+  ];
+
+  return (
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+      {tools.map((tool) => (
+        <Card
+          key={tool.title}
+          title={tool.title}
+          description={tool.description}
+          info={tool.info}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ToolsSection;
