f5b09ce5658ba68782c9a5c5fdbb2e32db68204a
Kyaw Thu <evanch98@gmail.com>
2023-08-08 14:13:03 +0800

    update the Subheading component

--- components/common/subheading.tsx
+++ components/common/subheading.tsx
@@ -1,5 +1,24 @@
-const Subheading = () => {
-  return <div>Subheading</div>;
+import Link from "next/link";
+
+interface SubheadingProps {
+  title: string;
+  moreHref?: string; // if there is more to see, provide the href for listings page
+}
+
+const Subheading: React.FC<SubheadingProps> = ({
+  title,
+  moreHref,
+}) => {
+  return (
+    <div className="flex items-center justify-between w-full">
+      <h2 className="text-xl md:text-2xl lg:text-3xl font-bold">{title}</h2>
+      {moreHref && (
+        <Link href={moreHref} className="hover:opacity-70">
+          See all
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default Subheading;
