de8724632164c999848a1b7a68ced87221617955
Kyaw Thu <evanch98@gmail.com>
2023-08-08 15:56:33 +0800

    update the AboutClient component

--- app/(pages)/about/about-client.tsx
+++ app/(pages)/about/about-client.tsx
@@ -1,5 +1,18 @@
+"use client";
+
+import Banner from "@/components/common/banner";
+
 const AboutClient = () => {
-  return <div>About</div>;
+  return (
+    <div className="w-full h-full mt-[70px] mb-5 flex items-center justify-center">
+      <div className="h-full w-full max-w-[1024px] flex flex-col items-center justify-center px-2 lg:px-0 gap-5">
+        <Banner
+          heading="About Me"
+          description="An aspiring front-end developer"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default AboutClient;
