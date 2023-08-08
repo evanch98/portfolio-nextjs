a7130879f5121d2688ed943419dae912d513f7e6
Kyaw Thu <evanch98@gmail.com>
2023-08-08 14:05:06 +0800

    update the HomeClient component

--- app/(root)/home-client.tsx
+++ app/(root)/home-client.tsx
@@ -1,16 +1,20 @@
 "use client";
 
-import Banner from "../../components/common/banner";
+import Subheading from "@/components/common/subheading";
+import Banner from "@/components/common/banner";
 
 const HomeClient = () => {
   return (
     <div className="w-full h-full mt-[70px] flex items-center justify-center">
-      <div className="h-full w-full max-w-[1024px] flex flex-col items-center justify-center px-2 lg:px-0">
+      <div className="h-full w-full max-w-[1024px] flex flex-col items-center justify-center px-2 lg:px-0 gap-5">
         <Banner
           heading="Kyaw Thu"
           description="An aspiring front-end developer"
           home={true}
         />
+        <section className="w-full">
+          <Subheading />
+        </section>
       </div>
     </div>
   );
