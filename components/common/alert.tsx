242f2e650588c15c88a13065590133e3ba8ed74c
Kyaw Thu <evanch98@gmail.com>
2023-08-08 20:13:52 +0800

    update the Alert component

--- components/common/alert.tsx
+++ components/common/alert.tsx
@@ -1,5 +1,9 @@
-const Alert = () => {
-  return <div>Alert</div>;
+interface AlertProps {
+  children: React.ReactNode;
+}
+
+const Alert: React.FC<AlertProps> = ({ children }) => {
+  return <div className="w-full bg-[#FCA5A5] rounded-2xl p-5">{children}</div>;
 };
 
 export default Alert;
