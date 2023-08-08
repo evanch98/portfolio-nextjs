96065c2c09c711d6a2b9af4b23d2a56879c1822e
Kyaw Thu <evanch98@gmail.com>
2023-08-08 14:49:46 +0800

    update the projectsSection component

--- app/(root)/components/projects-section.tsx
+++ app/(root)/components/projects-section.tsx
@@ -29,7 +29,7 @@ const ProjectsSection = () => {
   ];
 
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-5">
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
       {projects.slice(0, 3).map((project) => (
         <Card
           key={project.title}
