46181a1dd9f21e00d7aa4f16c0c1c1190570c988
Kyaw Thu <evanch98@gmail.com>
2023-08-08 14:31:52 +0800

    update the Card component

--- components/common/card.tsx
+++ components/common/card.tsx
@@ -1,5 +1,14 @@
 const Card = () => {
-  return <div>Card</div>;
+  return (
+    <div className="w-full bg-[#282B30] p-5 rounded-2xl flex flex-col gap-y-2">
+      <h3 className="md:text-lg lg:text-xl font-bold">Airbnb Clone</h3>
+      <p className="opacity-70 text-sm md:text-base">
+        An airbnb clone website built with Next JS, TypeScript, Tailwind CSS,
+        Prisma, MongoDB, and Zustand.
+      </p>
+      <div className="flex items-center justify-center gap-x-4"></div>
+    </div>
+  );
 };
 
 export default Card;
