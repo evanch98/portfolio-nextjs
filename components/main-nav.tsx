100b9c75c831a7df99b3bf8d2235a6498fa83361
Kyaw Thu <evanch98@gmail.com>
2023-08-08 14:32:47 +0800

    update the main-nav.tsx

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { FiExternalLink, FiHome, FiList, FiUser } from "react-icons/fi";
 import { twMerge } from "tailwind-merge";
-import NavLink from "./common/nav-link";
+import NavLink from "@/components/common/nav-link";
 
 export function MainNav({
   className,
