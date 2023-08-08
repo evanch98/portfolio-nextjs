d7a57ef3c10f25b0bacb3d3839e77873e396738a
Kyaw Thu <evanch98@gmail.com>
2023-08-09 03:03:03 +0800

    update the ContactSection component

--- app/(pages)/about/components/contact-section.tsx
+++ app/(pages)/about/components/contact-section.tsx
@@ -14,8 +14,10 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 import * as z from "zod";
 
 const formSchema = z.object({
@@ -32,6 +34,7 @@ type formValues = z.infer<typeof formSchema>;
 
 const ContactSection = () => {
   const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -49,8 +52,10 @@ const ContactSection = () => {
         "https://getform.io/f/7d7e9421-700a-4548-8a97-e472b180613d",
         data
       );
+      router.refresh();
+      toast.success("Message sent.");
     } catch (e) {
-      console.log(e);
+      toast.error("Something went wrong.");
     } finally {
       setLoading(false);
     }
