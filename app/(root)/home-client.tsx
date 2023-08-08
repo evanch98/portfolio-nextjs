2227acda8cecd189d19989a3a959de75566b008a
Kyaw Thu <evanch98@gmail.com>
2023-08-08 13:59:57 +0800

    update the HomeClient component

--- app/(root)/home-client.tsx
+++ app/(root)/home-client.tsx
@@ -6,7 +6,11 @@ const HomeClient = () => {
   return (
     <div className="w-full h-full mt-[70px] flex items-center justify-center">
       <div className="h-full w-full max-w-[1024px] flex flex-col items-center justify-center px-2 lg:px-0">
-        <Banner />
+        <Banner
+          heading="Kyaw Thu"
+          description="An aspiring front-end developer"
+          home={true}
+        />
       </div>
     </div>
   );
