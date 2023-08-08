45b20b52d2d575b35afda8dc206c341e9fb32328
Kyaw Thu <evanch98@gmail.com>
2023-08-08 14:42:50 +0800

    update the Card component

--- components/common/card.tsx
+++ components/common/card.tsx
@@ -2,24 +2,41 @@ import { FiGithub, FiGlobe, FiInfo } from "react-icons/fi";
 import NavLink from "./nav-link";
 import Link from "next/link";
 
-const Card = () => {
+interface CardProps {
+  title: string;
+  description: string;
+  code?: string;
+  live?: string;
+  info?: string;
+}
+
+const Card: React.FC<CardProps> = ({
+  title,
+  description,
+  code,
+  live,
+  info,
+}) => {
   return (
     <div className="w-full bg-[#282B30] p-5 rounded-2xl flex flex-col gap-y-2">
-      <h3 className="md:text-lg lg:text-xl font-bold">Airbnb Clone</h3>
-      <p className="opacity-70 text-sm md:text-base">
-        An airbnb clone website built with Next JS, TypeScript, Tailwind CSS,
-        Prisma, MongoDB, and Zustand.
-      </p>
+      <h3 className="md:text-lg lg:text-xl font-bold">{title}</h3>
+      <p className="opacity-70 text-sm md:text-base">{description}</p>
       <div className="flex gap-x-4 w-full items-center justify-center">
-        <Link href="/" className="hover:opacity-70 transition-opacity">
-          <NavLink label="Code" icon={FiGithub} />
-        </Link>
-        <Link href="/" className="hover:opacity-70 transition-opacity">
-          <NavLink label="Live" icon={FiGlobe} />
-        </Link>
-        <Link href="/" className="hover:opacity-70 transition-opacity">
-          <NavLink label="Info" icon={FiInfo} />
-        </Link>
+        {code && (
+          <Link href={code} className="hover:opacity-70 transition-opacity">
+            <NavLink label="Code" icon={FiGithub} />
+          </Link>
+        )}
+        {live && (
+          <Link href={live} className="hover:opacity-70 transition-opacity">
+            <NavLink label="Live" icon={FiGlobe} />
+          </Link>
+        )}
+        {info && (
+          <Link href={info} className="hover:opacity-70 transition-opacity">
+            <NavLink label="Info" icon={FiInfo} />
+          </Link>
+        )}
       </div>
     </div>
   );
