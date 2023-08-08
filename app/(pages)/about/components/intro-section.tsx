3a0433d3a1b8a0e2f6ed8146cd9a1dec56330814
Kyaw Thu <evanch98@gmail.com>
2023-08-08 16:06:34 +0800

    update the IntroSection component

--- app/(pages)/about/components/intro-section.tsx
+++ app/(pages)/about/components/intro-section.tsx
@@ -1,5 +1,30 @@
 const IntroSection = () => {
-  return <div>Intro</div>;
+  return (
+    <div className="w-full p-5 rounded-2xl bg-[#282B30] flex flex-col gap-y-2">
+      <p>
+        I have spent 7 years studying medicine at the University of Medicine 1,
+        Yangon. Due to several reasons, including pandemic, I had to drop out of
+        the university. For as long as I can remember, I have always been into
+        technology and working with computers. In 2021, I started learning
+        programming with Python, which made me love programming even more.
+      </p>
+      <p>
+        After exploring several career options, I was quickly drawn to front-end
+        web development. I started learning HTML and CSS to build simple
+        websites. Fascinated with how intricate web development can be, I was
+        quickly drawn to learn more. I started learning JavaScript and was even
+        more enthused with making websites interactive. I am now spending my
+        time building projects with React JS, Next JS, Tailwind CSS, and MongoDB
+        and learning new technologies.
+      </p>
+      <p>
+        Apart from coding, I enjoy swimming. My favorite movie and TV show
+        genres are Sci-fi and Sitcom. My favorite sport is soccer. My favorite
+        team is Liverpool FC based in Liverpool, England. I also watch NBA
+        occasionally. My favorite NBA team is Golden State Warriors.
+      </p>
+    </div>
+  );
 };
 
 export default IntroSection;
