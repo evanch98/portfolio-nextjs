5985450bae14393886cb8d2d0493cfa48cbb23dd
Kyaw Thu <evanch98@gmail.com>
2023-08-09 03:07:16 +0800

    update the ToastProvider

--- providers/toast-provider.tsx
+++ providers/toast-provider.tsx
@@ -3,5 +3,14 @@
 import { Toaster } from "react-hot-toast";
 
 export const ToastProvider = () => {
-  return <Toaster />;
+  return (
+    <Toaster
+      toastOptions={{
+        style: {
+          background: "#1E2124",
+          color: "white",
+        },
+      }}
+    />
+  );
 };
