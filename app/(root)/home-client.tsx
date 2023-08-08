af1f74977ba030dc562a6a488ff818eba3b57313
Kyaw Thu <evanch98@gmail.com>
2023-08-08 15:32:10 +0800

    update the HomeClient component

--- app/(root)/home-client.tsx
+++ app/(root)/home-client.tsx
@@ -7,7 +7,7 @@ import ToolsSection from "./components/tools-section";
 
 const HomeClient = () => {
   return (
-    <div className="w-full h-full mt-[70px] flex items-center justify-center">
+    <div className="w-full h-full mt-[70px] mb-5 flex items-center justify-center">
       <div className="h-full w-full max-w-[1024px] flex flex-col items-center justify-center px-2 lg:px-0 gap-5">
         <Banner
           heading="Kyaw Thu"
