4110dc4285ca804faaa9a1cce3a74c4f58d13e3d
Kyaw Thu <evanch98@gmail.com>
2023-08-11 19:47:44 +0800

    update the NotFound component

--- components/not-found.tsx
+++ components/not-found.tsx
@@ -1,8 +1,16 @@
+"use client";
+
+import Banner from "@/components/common/banner";
+
 const NotFound = () => {
   return (
     <div className="w-full h-full mt-[70px] mb-5 flex items-center justify-center">
       <div className="h-full w-full max-w-[1024px] flex flex-col items-center justify-center px-2 lg:px-0 gap-5">
-        NotFound
+        <Banner
+          heading="Something's wrong here..."
+          description="Sorry, but the page that you requested doesn't exist."
+          home={false}
+        />
       </div>
     </div>
   );
