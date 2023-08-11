c1299ddf19b26ae47f7f1b23fed8b6110568b177
Kyaw Thu <evanch98@gmail.com>
2023-08-11 19:27:29 +0800

    update the LinksClient

--- app/(pages)/links/links-client.tsx
+++ app/(pages)/links/links-client.tsx
@@ -1,10 +1,20 @@
 "use client";
 
+import Banner from "@/components/common/banner";
+import Subheading from "@/components/common/subheading";
+
 const LinksClient = () => {
   return (
     <div className="w-full h-full mt-[70px] mb-5 flex items-center justify-center">
       <div className="h-full w-full max-w-[1024px] flex flex-col items-center justify-center px-2 lg:px-0 gap-5">
-        LinksClient
+        <Banner
+          heading="Links"
+          description="Checkout out my social media links"
+          home={false}
+        />
+        <section className="w-full flex flex-col gap-5">
+          <Subheading title="Links" />
+        </section>
       </div>
     </div>
   );
