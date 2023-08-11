8da4a1dc35b73fb23b6d7667e8057df7c471f01b
Kyaw Thu <evanch98@gmail.com>
2023-08-11 16:46:54 +0800

    update the OverviewSection

--- app/(pages)/projects/[projectId]/components/overview-section.tsx
+++ app/(pages)/projects/[projectId]/components/overview-section.tsx
@@ -1,7 +1,12 @@
+import NavLink from "@/components/common/nav-link";
+import Link from "next/link";
+import { FiGithub, FiGlobe } from "react-icons/fi";
+
 interface OverviewSectionProps {
   title: string;
   category: string;
   date: string;
+  framework: string;
   gitHub: string;
   live: string;
 }
@@ -10,16 +15,35 @@ const OverviewSection: React.FC<OverviewSectionProps> = ({
   title,
   category,
   date,
+  framework,
   gitHub,
   live,
 }) => {
   return (
-    <div className="w-full grid grid-cols-1 gap-5">
+    <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="w-full bg-[#282B30] p-5 rounded-2xl flex flex-col gap-y-2 shadow-lg">
         <h2 className="md:text-lg lg:text-xl font-bold">{title}</h2>
-        <p>{category}</p>
+        <p>{framework}</p>
+        <p className="opacity-70 text-sm md:text-base">{category}</p>
         <p className="text-xs md:text-sm italic opacity-70">
           Start Date: {date}
+        </p>
+      </div>
+      <div className="w-full bg-[#282B30] p-5 rounded-2xl flex flex-col gap-y-2 shadow-lg">
+        <h2 className="md:text-lg lg:text-xl font-bold">Links</h2>
+        <div className="w-full flex gap-x-4">
+          <Link href={gitHub} target="_blank">
+            <NavLink label="Code" icon={FiGithub} />
+          </Link>
+          <Link href={live} target="_blank">
+            <NavLink label="Live" icon={FiGlobe} />
+          </Link>
+        </div>
+        <p className="text-xs md:text-sm opacity-70 italic">
+          Code is for the source code
+        </p>
+        <p className="text-xs md:text-sm opacity-70 italic">
+          Live is for the live demo website
         </p>
       </div>
     </div>
