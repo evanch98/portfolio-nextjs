979b7e363750759767a6b2cedf44de21546be452
Kyaw Thu <evanch98@gmail.com>
2023-08-11 15:16:08 +0800

    update the projectType

--- types/types.ts
+++ types/types.ts
@@ -1,13 +1,8 @@
-import { StaticImport } from "next/dist/shared/lib/get-img-props";
-
 export interface projectType {
   title: string;
   description: string[];
-  imageSrc: StaticImport;
-  imageAlt: string;
   framework: string;
   techStack: string[];
   gitHubLink: string;
   demoLink: string;
-  pageUrl: string;
 }
