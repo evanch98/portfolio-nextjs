8ad1ec5f0581475b56335a6264b0b5ee17a95f35
Kyaw Thu <evanch98@gmail.com>
2023-08-11 17:53:19 +0800

    move the contact-section.tsx

--- app/(pages)/about/about-client.tsx
+++ app/(pages)/about/about-client.tsx
@@ -3,7 +3,7 @@
 import Banner from "@/components/common/banner";
 import Subheading from "@/components/common/subheading";
 import IntroSection from "./components/intro-section";
-import ContactSection from "./components/contact-section";
+import ContactSection from "../../../components/common/contact-section";
 
 const AboutClient = () => {
   return (
