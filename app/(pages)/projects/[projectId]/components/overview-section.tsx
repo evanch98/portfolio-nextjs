0b06fb3e115e9210369a89e0d72cdf3c36eb2975
Kyaw Thu <evanch98@gmail.com>
2023-08-11 16:31:17 +0800

    update the OverviewSection

--- app/(pages)/projects/[projectId]/components/overview-section.tsx
+++ app/(pages)/projects/[projectId]/components/overview-section.tsx
@@ -16,7 +16,11 @@ const OverviewSection: React.FC<OverviewSectionProps> = ({
   return (
     <div className="w-full grid grid-cols-1 gap-5">
       <div className="w-full bg-[#282B30] p-5 rounded-2xl flex flex-col gap-y-2 shadow-lg">
-        <h2 className="">{title}</h2>
+        <h2 className="md:text-lg lg:text-xl font-bold">{title}</h2>
+        <p>{category}</p>
+        <p className="text-xs md:text-sm italic opacity-70">
+          Start Date: {date}
+        </p>
       </div>
     </div>
   );
