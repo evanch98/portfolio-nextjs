665823054456fed93305f39e34c706a8a821ebd2
Kyaw Thu <evanch98@gmail.com>
2023-08-11 16:50:00 +0800

    add a new component DescSection

---
+++ app/(pages)/projects/[projectId]/components/desc-section.tsx
@@ -0,0 +1,9 @@
+interface DescSectionProps {
+  descriptions: string[];
+}
+
+const DescSection: React.FC<DescSectionProps> = ({ descriptions }) => {
+  return <div>DescSection</div>;
+};
+
+export default DescSection;
