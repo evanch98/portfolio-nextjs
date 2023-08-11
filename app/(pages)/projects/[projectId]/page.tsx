dde6fa1f839c8933eecf1f34e68e9c96b74bff22
Kyaw Thu <evanch98@gmail.com>
2023-08-11 15:45:38 +0800

    update the ProjectPage

--- app/(pages)/projects/[projectId]/page.tsx
+++ app/(pages)/projects/[projectId]/page.tsx
@@ -1,9 +1,12 @@
+import { getProject } from "@/actions/get-project";
 import ProjectClient from "./project-client";
 
 const ProjectPage = ({ params }: { params: { projectId: string } }) => {
+  const project = getProject(params.projectId);
+
   return (
     <div>
-      <ProjectClient />
+      <ProjectClient data={project} />
     </div>
   );
 };
