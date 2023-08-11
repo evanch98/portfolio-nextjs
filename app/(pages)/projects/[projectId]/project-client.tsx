4bd93e198ef6883ee032e91330c154a764f6f3ff
Kyaw Thu <evanch98@gmail.com>
2023-08-11 15:58:07 +0800

    update the ProjectClient component

--- app/(pages)/projects/[projectId]/project-client.tsx
+++ app/(pages)/projects/[projectId]/project-client.tsx
@@ -1,8 +1,17 @@
-const ProjectClient = () => {
+"use client";
+
+import Banner from "@/components/common/banner";
+import { projectType } from "@/types/types";
+
+interface ProjectClientProps {
+  data: projectType;
+}
+
+const ProjectClient: React.FC<ProjectClientProps> = ({ data }) => {
   return (
     <div className="w-full h-full mt-[70px] mb-5 flex items-center justify-center">
       <div className="h-full w-full max-w-[1024px] flex flex-col items-center justify-center px-2 lg:px-0 gap-5">
-        Project Client
+        <Banner heading={data.title} description={data.description[0]} />
       </div>
     </div>
   );
