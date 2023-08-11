fe0eb71ecbd0ea17038f983af711e505b7ae351a
Kyaw Thu <evanch98@gmail.com>
2023-08-11 16:03:01 +0800

    update the ProjectClient component

--- app/(pages)/projects/[projectId]/project-client.tsx
+++ app/(pages)/projects/[projectId]/project-client.tsx
@@ -11,7 +11,11 @@ const ProjectClient: React.FC<ProjectClientProps> = ({ data }) => {
   return (
     <div className="w-full h-full mt-[70px] mb-5 flex items-center justify-center">
       <div className="h-full w-full max-w-[1024px] flex flex-col items-center justify-center px-2 lg:px-0 gap-5">
-        <Banner heading={data.title} description={data.description[0]} />
+        <Banner
+          heading={data.title}
+          description={data.description[0]}
+          home={false}
+        />
       </div>
     </div>
   );
