abc53760e3e53571e0a2722e1e33605db4b5b94c
Kyaw Thu <evanch98@gmail.com>
2023-08-11 18:20:37 +0800

    update the Banner component

--- components/common/banner.tsx
+++ components/common/banner.tsx
@@ -2,6 +2,7 @@ import GitHubIcon from "@/components/common/icons/GitHubIcon";
 import LinkedinIcon from "@/components/common/icons/LinkedinIcon";
 import MailIcon from "@/components/common/icons/MailIcon";
 import ResumeIcon from "@/components/common/icons/ResumeIcon";
+import { motion } from "framer-motion";
 
 interface BannerProps {
   heading: string;
@@ -9,17 +10,43 @@ interface BannerProps {
   home?: boolean;
 }
 
+const mainVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { duration: 1, staggerChildren: 0.1, delayChildren: 0.3 },
+  },
+};
+
+const childVariants = {
+  hidden: { y: "-50vh" },
+  visible: { y: 0, transition: { duration: 1, type: "spring" } },
+};
+
 const Banner: React.FC<BannerProps> = ({
   heading,
   description,
   home = true,
 }) => {
   return (
-    <div className="w-full p-5 flex flex-col items-center justify-center bg-[#282B30] rounded-2xl gap-y-2 shadow-lg">
-      <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">{heading}</h1>
-      <p className="opacity-70 md:text-lg lg:text-xl text-center">
+    <motion.div
+      className="w-full p-5 flex flex-col items-center justify-center bg-[#282B30] rounded-2xl gap-y-2 shadow-lg"
+      variants={mainVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.h1
+        className="text-2xl md:text-3xl lg:text-4xl font-bold"
+        variants={childVariants}
+      >
+        {heading}
+      </motion.h1>
+      <motion.p
+        className="opacity-70 md:text-lg lg:text-xl text-center"
+        variants={childVariants}
+      >
         {description}
-      </p>
+      </motion.p>
       {home && (
         <div className="flex items-center justify-center gap-x-4">
           <GitHubIcon className="hover:opacity-70 transition-opacity" />
@@ -28,7 +55,7 @@ const Banner: React.FC<BannerProps> = ({
           <ResumeIcon className="hover:opacity-70 transition-opacity" />
         </div>
       )}
-    </div>
+    </motion.div>
   );
 };
 
