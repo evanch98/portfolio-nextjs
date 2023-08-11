a263f59c82eef2eb948d3889f5c99630df283f52
Kyaw Thu <evanch98@gmail.com>
2023-08-11 16:26:02 +0800

    update the OverviewSection

--- app/(pages)/projects/[projectId]/components/overview-section.tsx
+++ app/(pages)/projects/[projectId]/components/overview-section.tsx
@@ -1,5 +1,25 @@
-const OverviewSection = () => {
-  return <div>Overview</div>;
+interface OverviewSectionProps {
+  title: string;
+  category: string;
+  date: string;
+  gitHub: string;
+  live: string;
+}
+
+const OverviewSection: React.FC<OverviewSectionProps> = ({
+  title,
+  category,
+  date,
+  gitHub,
+  live,
+}) => {
+  return (
+    <div className="w-full grid grid-cols-1 gap-5">
+      <div className="w-full bg-[#282B30] p-5 rounded-2xl flex flex-col gap-y-2 shadow-lg">
+        <h2 className="">{title}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default OverviewSection;
