a40b8a5f4763ea6d2ec3c0dbe4747976264a46f1
Kyaw Thu <evanch98@gmail.com>
2023-08-11 19:36:13 +0800

    update the LinksSection

--- app/(pages)/links/components/links-section.tsx
+++ app/(pages)/links/components/links-section.tsx
@@ -1,5 +1,50 @@
+import Card from "@/components/common/card";
+import { motion } from "framer-motion";
+
+const links = [
+  {
+    title: "GitHub",
+    description: "Checkout and star my projects",
+    info: "https://github.com/evanch98",
+  },
+  {
+    title: "LinkedIn",
+    description: "Checkout my LinkedIn profile",
+    info: "https://www.linkedin.com/in/kyaw-thu-0b3956212/",
+  },
+  {
+    title: "Discord",
+    description: "Let's chat on Discord",
+    info: "https://www.discordapp.com/users/442602474067722241",
+  },
+];
+
+const mainVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { duration: 1, staggerChildren: 0.1, delayChildren: 0.3 },
+  },
+};
+
 const LinksSection = () => {
-  return <div>Links Section</div>;
+  return (
+    <motion.div
+      className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5"
+      variants={mainVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      {links.map((link) => (
+        <Card
+          key={link.title}
+          title={link.title}
+          description={link.description}
+          info={link.info}
+        />
+      ))}
+    </motion.div>
+  );
 };
 
 export default LinksSection;
