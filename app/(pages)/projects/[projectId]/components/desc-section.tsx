d7d3554bc5b23cddab6e8f6857c34afcecfbb915
Kyaw Thu <evanch98@gmail.com>
2023-08-11 16:54:46 +0800

    update the DescSection

--- app/(pages)/projects/[projectId]/components/desc-section.tsx
+++ app/(pages)/projects/[projectId]/components/desc-section.tsx
@@ -3,7 +3,17 @@ interface DescSectionProps {
 }
 
 const DescSection: React.FC<DescSectionProps> = ({ descriptions }) => {
-  return <div>DescSection</div>;
+  return (
+    <div className="w-full grid grid-cols-1 gap-5">
+      <div className="w-full bg-[#282B30] p-5 rounded-2xl flex flex-col gap-y-2 shadow-lg">
+        <ul className="list-inside list-disc">
+          {descriptions.map((description, index) => (
+            <li key={index}>{description}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default DescSection;
