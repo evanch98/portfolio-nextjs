ae44b2aae67cccf1ffc6a2db4564c490a16b2357
Kyaw Thu <evanch98@gmail.com>
2023-08-11 17:06:18 +0800

    update the ProjectsClient component

--- app/(pages)/projects/projects-client.tsx
+++ app/(pages)/projects/projects-client.tsx
@@ -1,7 +1,21 @@
 "use client";
 
+import Banner from "@/components/common/banner";
+import ProjectsSection from "@/components/common/projects-section";
+import Subheading from "@/components/common/subheading";
+
 const ProjectsClient = () => {
-  return <div>Projects Client</div>;
+  return (
+    <div className="w-full h-full mt-[70px] mb-5 flex items-center justify-center">
+      <div className="h-full w-full max-w-[1024px] flex flex-col items-center justify-center px-2 lg:px-0 gap-5">
+        <Banner heading="Projects" description="Checkout my projects" />
+        <section className="w-full flex flex-col gap-5">
+          <Subheading title="Projects" />
+          <ProjectsSection isAll />
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectsClient;
