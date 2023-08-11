b61e3c1a2b8d134eb6f266bba7a718f96109a455
Kyaw Thu <evanch98@gmail.com>
2023-08-11 18:24:59 +0800

    update the ProjectsSection component

--- components/common/projects-section.tsx
+++ components/common/projects-section.tsx
@@ -1,15 +1,29 @@
 import { getProjects } from "@/actions/get-projects";
+import { motion } from "framer-motion";
 import Card from "@/components/common/card";
 
 interface ProjectsSectionProps {
   isAll?: boolean;
 }
 
+const mainVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { duration: 1, staggerChildren: 0.1, delayChildren: 0.3 },
+  },
+};
+
 const ProjectsSection: React.FC<ProjectsSectionProps> = ({ isAll = false }) => {
   const projects = isAll ? getProjects() : getProjects().slice(0, 3);
 
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+    <motion.div
+      className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5"
+      variants={mainVariants}
+      initial="hidden"
+      animate="visible"
+    >
       {projects.map((project) => (
         <Card
           key={project.id}
@@ -20,7 +34,7 @@ const ProjectsSection: React.FC<ProjectsSectionProps> = ({ isAll = false }) => {
           info={`/projects/${project.id}`}
         />
       ))}
-    </div>
+    </motion.div>
   );
 };
 
