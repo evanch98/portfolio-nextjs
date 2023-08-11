4b23817f9a6302d47b9a04d59886682c167b928d
Kyaw Thu <evanch98@gmail.com>
2023-08-11 15:44:16 +0800

    update the ProjectClient component

--- app/(pages)/projects/[projectId]/project-client.tsx
+++ app/(pages)/projects/[projectId]/project-client.tsx
@@ -1,5 +1,11 @@
 const ProjectClient = () => {
-  return <div>Project Client</div>;
+  return (
+    <div className="w-full h-full mt-[70px] mb-5 flex items-center justify-center">
+      <div className="h-full w-full max-w-[1024px] flex flex-col items-center justify-center px-2 lg:px-0 gap-5">
+        Project Client
+      </div>
+    </div>
+  );
 };
 
 export default ProjectClient;
