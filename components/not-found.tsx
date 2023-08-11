9bcfd54e3b4ef45beda7641f38214c841341fee6
Kyaw Thu <evanch98@gmail.com>
2023-08-11 19:43:30 +0800

    update the NotFound component

--- components/not-found.tsx
+++ components/not-found.tsx
@@ -1,5 +1,11 @@
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="w-full h-full mt-[70px] mb-5 flex items-center justify-center">
+      <div className="h-full w-full max-w-[1024px] flex flex-col items-center justify-center px-2 lg:px-0 gap-5">
+        NotFound
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
