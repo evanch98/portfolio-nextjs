03ea8ff37d74ceddc97db5c496ffce40cdecd742
Kyaw Thu <evanch98@gmail.com>
2023-08-11 19:08:33 +0800

    update the OverviewSection

--- app/(pages)/projects/[projectId]/components/overview-section.tsx
+++ app/(pages)/projects/[projectId]/components/overview-section.tsx
@@ -1,6 +1,7 @@
 import NavLink from "@/components/common/nav-link";
 import Link from "next/link";
 import { FiGithub, FiGlobe } from "react-icons/fi";
+import { motion } from "framer-motion";
 
 interface OverviewSectionProps {
   title: string;
@@ -11,6 +12,27 @@ interface OverviewSectionProps {
   live: string;
 }
 
+const mainVariants = {
+  hidden: { x: "-100vw" },
+  visible: {
+    x: 0,
+    transition: {
+      duration: 1,
+      delayChildren: 0.3,
+      staggerChildren: 0.1,
+      type: "spring",
+    },
+  },
+};
+
+const childVariants = {
+  hidden: { x: "-100vw" },
+  visible: {
+    x: 0,
+    transition: { duration: 1, type: "spring" },
+  },
+};
+
 const OverviewSection: React.FC<OverviewSectionProps> = ({
   title,
   category,
@@ -20,18 +42,46 @@ const OverviewSection: React.FC<OverviewSectionProps> = ({
   live,
 }) => {
   return (
-    <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-5">
-      <div className="w-full bg-[#282B30] p-5 rounded-2xl flex flex-col gap-y-2 shadow-lg">
-        <h2 className="md:text-lg lg:text-xl font-bold">{title}</h2>
-        <p>{framework}</p>
-        <p className="opacity-70 text-sm md:text-base">{category}</p>
-        <p className="text-xs md:text-sm italic opacity-70">
+    <motion.div
+      className="w-full grid grid-cols-1 md:grid-cols-2 gap-5"
+      initial="hidden"
+      animate="visible"
+    >
+      <motion.div
+        className="w-full bg-[#282B30] p-5 rounded-2xl flex flex-col gap-y-2 shadow-lg"
+        variants={mainVariants}
+      >
+        <motion.h2
+          className="md:text-lg lg:text-xl font-bold"
+          variants={childVariants}
+        >
+          {title}
+        </motion.h2>
+        <motion.p variants={childVariants}>{framework}</motion.p>
+        <motion.p
+          className="opacity-70 text-sm md:text-base"
+          variants={childVariants}
+        >
+          {category}
+        </motion.p>
+        <motion.p
+          className="text-xs md:text-sm italic opacity-70"
+          variants={childVariants}
+        >
           Start Date: {date}
-        </p>
-      </div>
-      <div className="w-full bg-[#282B30] p-5 rounded-2xl flex flex-col gap-y-2 shadow-lg">
-        <h2 className="md:text-lg lg:text-xl font-bold">Links</h2>
-        <div className="w-full flex gap-x-4">
+        </motion.p>
+      </motion.div>
+      <motion.div
+        className="w-full bg-[#282B30] p-5 rounded-2xl flex flex-col gap-y-2 shadow-lg"
+        variants={mainVariants}
+      >
+        <motion.h2
+          className="md:text-lg lg:text-xl font-bold"
+          variants={childVariants}
+        >
+          Links
+        </motion.h2>
+        <motion.div className="w-full flex gap-x-4" variants={childVariants}>
           <Link
             href={gitHub}
             target="_blank"
@@ -46,15 +96,21 @@ const OverviewSection: React.FC<OverviewSectionProps> = ({
           >
             <NavLink label="Live" icon={FiGlobe} />
           </Link>
-        </div>
-        <p className="text-xs md:text-sm opacity-70 italic">
+        </motion.div>
+        <motion.p
+          className="text-xs md:text-sm opacity-70 italic"
+          variants={childVariants}
+        >
           Code is for the source code
-        </p>
-        <p className="text-xs md:text-sm opacity-70 italic">
+        </motion.p>
+        <motion.p
+          className="text-xs md:text-sm opacity-70 italic"
+          variants={childVariants}
+        >
           Live is for the live demo website
-        </p>
-      </div>
-    </div>
+        </motion.p>
+      </motion.div>
+    </motion.div>
   );
 };
 
