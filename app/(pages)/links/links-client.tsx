f703028dd2a0cf045dd51c9b713fdb1a804c79ba
Kyaw Thu <evanch98@gmail.com>
2023-08-11 19:24:24 +0800

    add a new component LinksClient

---
+++ app/(pages)/links/links-client.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+const LinksClient = () => {
+  return (
+    <div className="w-full h-full mt-[70px] mb-5 flex items-center justify-center">
+      <div className="h-full w-full max-w-[1024px] flex flex-col items-center justify-center px-2 lg:px-0 gap-5">
+        LinksClient
+      </div>
+    </div>
+  );
+};
+
+export default LinksClient;
