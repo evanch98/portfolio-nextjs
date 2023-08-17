998d5004492890272eb9a280de804ad0d1f87bbd
Kyaw Thu <evanch98@gmail.com>
2023-08-17 23:39:10 +0800

    add a new project

--- utils/projects.ts
+++ utils/projects.ts
@@ -21,6 +21,7 @@ export const airbnbProject: projectType = {
     "MongoDB",
     "Prisma",
     "NextAuth",
+    "Cloudinary",
   ],
   date: "May 8 2023",
   category: "Hobby Project",
@@ -55,6 +56,38 @@ export const spotifyProject: projectType = {
   demoLink: "https://spotify-clone-nextjs-xi.vercel.app/",
 };
 
-const projects: projectType[] = [spotifyProject, airbnbProject];
+export const eCommerceAdmin: projectType = {
+  id: "eCommerceAdmin",
+  title: "E-Commerce Admin Dashboard",
+  description: [
+    "Built using cutting-edge technologies like Next JS, TypeScript, Tailwind CSS, Shadcn UI, Planet Scale, Prisma, Zustand, Clerk Auth, and Cloudinary",
+    "The admin dashboard allows the website's administrators to create, update, and delete store information.",
+    "Admins can efficiently manage the billboards or banners that appear on the website's homepage. They can upload, update, and remove promotional banners to attract customers and highlight ongoing offers or sales.",
+    "With the admin dashboard, managing product categories, sizes, and colors becomes a breeze. Administrators can add, edit, or delete categories, sizes, and colors, enabling the website to efficiently organize and display products.",
+    "To enable smooth communication between the front-end web application and the back-end, the dashboard incorporates API routes. This ensures that product information is seamlessly fetched and displayed on the customer-facing website.",
+    "The admin dashboard utilizes Clerk auth for secure authentication, ensuring that only authorized personnel can access and manage sensitive information.",
+    "With Planet Scale and Prisma integration, the application benefits from a robust and scalable database, capable of handling growing product inventories and increasing website traffic.",
+    "The heart of any e-commerce website lies in its product management capabilities. The admin dashboard empowers administrators to add new products, update existing ones, archive out-of-stock products, mark products as featured products, and remove products that are no longer available. They can set product details such as name, category, price, images, and other product-specific attributes.",
+    "Efficient order management is crucial for any e-commerce website. The dashboard provides comprehensive tools to view, process, and manage incoming orders.",
+  ],
+  framework: "Next JS",
+  techStack: [
+    "Next JS",
+    "TypeScript",
+    "Tailwind CSS",
+    "Shadcn UI",
+    "Planet Scale",
+    "Prisma",
+    "Zustand",
+    "Clerk Auth",
+    "Cloudinary",
+  ],
+  date: "July 26 2023",
+  category: "Hobby Project",
+  gitHubLink: "https://github.com/evanch98/ecommerce-admin-nextjs",
+  demoLink: "",
+};
+
+const projects: projectType[] = [eCommerceAdmin, spotifyProject, airbnbProject];
 
 export default projects;
