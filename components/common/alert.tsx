f779840f07ae5cb9eb751479e9703d4b4ef9168c
Kyaw Thu <evanch98@gmail.com>
2023-08-24 16:17:10 +0800

    update the Alert component

--- components/common/alert.tsx
+++ components/common/alert.tsx
@@ -1,9 +1,18 @@
+import { cn } from "@/lib/utils";
+
 interface AlertProps {
   children: React.ReactNode;
+  className?: string;
 }
 
-const Alert: React.FC<AlertProps> = ({ children }) => {
-  return <div className="w-full bg-[#FCA5A5] rounded-2xl p-5 shadow-lg">{children}</div>;
+const Alert: React.FC<AlertProps> = ({ children, className }) => {
+  return (
+    <div
+      className={cn("w-full bg-[#FCA5A5] rounded-2xl p-5 shadow-lg", className)}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Alert;
