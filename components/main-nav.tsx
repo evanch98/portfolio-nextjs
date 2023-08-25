7aebe19dd1acc36431dbcaf6af39f3e9482bff6e
Kyaw Thu <evanch98@gmail.com>
2023-08-25 13:18:49 +0800

    add comments

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -3,12 +3,16 @@
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { FiExternalLink, FiHome, FiList, FiUser } from "react-icons/fi";
-import { twMerge } from "tailwind-merge";
 import NavLink from "@/components/common/nav-link";
 import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
 
+/* Variants for the "Framer Motion" animation */
 const mainVariants = {
+  // Initial position
+  // x is -50% in both hidden and visible to maintain the translated position made with Tailwind CSS
   hidden: { y: "-100vh", x: "-50%" },
+  // Final position
   visible: { y: 0, x: "-50%", transition: { duration: 1, type: "spring" } },
 };
 
@@ -16,8 +20,11 @@ export function MainNav({
   className,
   ...props
 }: React.HTMLAttributes<HTMLElement>) {
-  const pathname = usePathname();
+  const pathname = usePathname(); // To check the pathname of the current url
+  const router = useRouter(); // To change the route
 
+  // Available routes in the website
+  // ***Subject to CHANGE in the future***
   const routes = [
     {
       href: "/",
@@ -41,8 +48,6 @@ export function MainNav({
     },
   ];
 
-  const router = useRouter();
-
   return (
     <motion.nav
       className="fixed top-0 left-1/2 -translate-x-1/2 px-5 py-2 flex items-center space-x-4 lg:space-x-6 bg-[#282B30] rounded-b-2xl shadow-lg z-50"
@@ -51,13 +56,14 @@ export function MainNav({
       animate="visible"
     >
       {routes.map((route) => (
+        // Whenever the user clicks on the link, refresh so that it behaves as intended on 404 page.
         <Link
           onClick={() => {
             router.refresh();
           }}
           key={route.href}
           href={route.href}
-          className={twMerge(
+          className={cn(
             "transition-colors hover:opacity-100",
             route.active ? "opacity-100" : "opacity-50"
           )}
