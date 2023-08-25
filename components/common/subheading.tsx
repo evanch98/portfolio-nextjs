896dfa3de32f31708ca58124ae0c06418f7d5362
Kyaw Thu <evanch98@gmail.com>
2023-08-25 13:26:51 +0800

    add comments

--- components/common/subheading.tsx
+++ components/common/subheading.tsx
@@ -1,10 +1,19 @@
 import Link from "next/link";
 
+/**
+ * Props for the Subheading functional component
+ */
 interface SubheadingProps {
   title: string;
   moreHref?: string; // if there is more to see, provide the href for listings page
 }
 
+/**
+ * This functional component is for the Subheading on each page.
+ * @param param0 accepts the title of the Subheading
+ * @param param1 accepts the link (optional)
+ * @returns the React component
+ */
 const Subheading: React.FC<SubheadingProps> = ({
   title,
   moreHref,
@@ -12,6 +21,7 @@ const Subheading: React.FC<SubheadingProps> = ({
   return (
     <div className="flex items-center justify-between w-full">
       <h2 className="text-xl md:text-2xl lg:text-3xl font-bold">{title}</h2>
+      {/* If the link is provided, it means the subheading requires (See all) */}
       {moreHref && (
         <Link href={moreHref} className="hover:opacity-70">
           See all
