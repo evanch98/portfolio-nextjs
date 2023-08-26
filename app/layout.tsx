7bbcbbe1bf379f417dfcdd9bbf713ed9e576cbf2
Kyaw Thu <evanch98@gmail.com>
2023-08-27 02:51:20 +0800

    update the RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { ToastProvider } from "@/providers/toast-provider";
 import { ThemeProvider } from "@/providers/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
 
 export const metadata: Metadata = {
   title: "Kyaw | Front-end developer",
@@ -18,8 +19,16 @@ export default function RootLayout({
     <html lang="en">
       <body className="relative">
         <ToastProvider />
+        <div className="absolute top-0 right-5">
+          <ModeToggle />
+        </div>
         <MainNav />
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          storageKey="portfolio"
+        >
           {children}
         </ThemeProvider>
       </body>
