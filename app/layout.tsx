35f4221d786f3ee02c34d92d315bfce9d5251f05
Kyaw Thu <evanch98@gmail.com>
2023-08-27 02:54:41 +0800

    update the RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { ToastProvider } from "@/providers/toast-provider";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { ModeToggle } from "@/components/mode-toggle";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "Kyaw | Front-end developer",
@@ -16,8 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className="relative">
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "relative",
+          "bg-white text-[#1E2124] dark:bg-[#1E2124] dark:text-white"
+        )}
+      >
         <ToastProvider />
         <div className="absolute top-0 right-5">
           <ModeToggle />
