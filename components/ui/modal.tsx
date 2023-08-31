62a24ba7f1b26e47661011b67c811c339a5288ed
Kyaw Thu <evanch98@gmail.com>
2023-08-31 20:29:48 +0800

    add a new component Modal

---
+++ components/ui/modal.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+const Modal = () => {
+  return <div>Modal</div>;
+};
+
+export default Modal;
