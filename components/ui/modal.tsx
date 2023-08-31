f8e43bde38fba0672563b209a45094602222d475
Kyaw Thu <evanch98@gmail.com>
2023-08-31 20:35:38 +0800

    update the Modal component

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -8,8 +8,39 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 
-const Modal = () => {
-  return <div>Modal</div>;
+interface ModalProps {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  title,
+  description,
+  isOpen,
+  onClose,
+  children,
+}) => {
+  const onChange = (open: boolean) => {
+    // if the open is false, it means the modal should be closed
+    if (!open) {
+      onClose();
+    }
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div>{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
 };
 
 export default Modal;
