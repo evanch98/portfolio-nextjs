47b50ae7155e14be850e0678783eaf9297c5ef29
Kyaw Thu <evanch98@gmail.com>
2023-08-31 20:54:00 +0800

    add ModalProvider

---
+++ providers/modal-provider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import UoPeopleModal from "@/components/modals/uopeople-modal";
+import { useEffect, useState } from "react";
+
+export const ModalProvider = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  });
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <UoPeopleModal />
+    </>
+  );
+};
