29ebf734a55956cc9619588088a6faab8b734afd
Kyaw Thu <evanch98@gmail.com>
2023-08-31 20:51:18 +0800

    update the UoPeopleModal

--- components/modals/uopeople-modal.tsx
+++ components/modals/uopeople-modal.tsx
@@ -1,7 +1,20 @@
 "use client";
 
+import { useUoPeopleModal } from "@/hooks/use-uopeople-modal";
+import Modal from "@/components/ui/modal";
+
 const UoPeopleModal = () => {
-  return <div>Edu Modal</div>;
+  const uoPeopleModal = useUoPeopleModal();
+  return (
+    <Modal
+      title="University of the People"
+      description="Bachelor of Science in Computer Science"
+      isOpen={uoPeopleModal.isOpen}
+      onClose={uoPeopleModal.onClose}
+    >
+      UoPeople
+    </Modal>
+  );
 };
 
 export default UoPeopleModal;
