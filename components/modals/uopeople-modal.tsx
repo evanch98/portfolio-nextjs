035ecea8088d6a830adcd786fae6bb3af05f9801
Kyaw Thu <evanch98@gmail.com>
2023-08-31 21:39:37 +0800

    update the UoPeopleModal

--- components/modals/uopeople-modal.tsx
+++ components/modals/uopeople-modal.tsx
@@ -1,8 +1,16 @@
 "use client";
 
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
 import { useUoPeopleModal } from "@/hooks/use-uopeople-modal";
 import Modal from "@/components/ui/modal";
 import { Separator } from "@/components/ui/separator";
+import { useState } from "react";
+import { Button } from "../ui/button";
+import { ChevronsUpDown } from "lucide-react";
 
 const courses = [
   {
@@ -45,6 +53,7 @@ const courses = [
 
 const UoPeopleModal = () => {
   const uoPeopleModal = useUoPeopleModal();
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <Modal
       title="University of the People"
@@ -58,17 +67,29 @@ const UoPeopleModal = () => {
           <p>Sep, 2021 - Mar, 2025 (Expected)</p>
         </div>
         <Separator className="bg-white/70 my-2" />
-        <div className="flex flex-col w-full">
-          <h2 className="font-bold">Courses</h2>
-          {courses.map((course) => (
-            <div
-              key={course.name}
-              className="flex items-center justify-between w-full mt-2 text-sm opacity-70"
-            >
-              <p>{course.name}</p>
-              <p>{course.gpa}</p>
+        <div className="w-full">
+          <Collapsible open={isOpen} onOpenChange={setIsOpen}>
+            <div className="flex items-center justify-between w-full">
+              <h2 className="font-bold">Courses</h2>
+              <CollapsibleTrigger asChild>
+                <Button variant="ghost" size="sm" className="w-9 p-0">
+                  <ChevronsUpDown className="h-4 w-4" />
+                  <span className="sr-only">Toggle</span>
+                </Button>
+              </CollapsibleTrigger>
             </div>
-          ))}
+            <CollapsibleContent>
+              {courses.map((course) => (
+                <div
+                  key={course.name}
+                  className="flex items-center justify-between w-full mt-2 text-sm opacity-70"
+                >
+                  <p>{course.name}</p>
+                  <p>{course.gpa}</p>
+                </div>
+              ))}
+            </CollapsibleContent>
+          </Collapsible>
         </div>
       </div>
     </Modal>
