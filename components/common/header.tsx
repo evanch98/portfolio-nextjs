2b15576e71e1aceacdb6cd961b544c06140fb328
Kyaw Thu <evanch98@gmail.com>
2023-09-20 18:08:58 +0800

    add a new component Header

---
+++ components/common/header.tsx
@@ -0,0 +1,13 @@
+interface HeaderProps {
+  title: string;
+  description: string;
+}
+
+export const Header = ({ title, description }: HeaderProps) => {
+  return (
+    <div className="w-full flex flex-col items-center justify-center text-center gap-y-[24px]">
+      <h1 className="heading-1 text-[--black] uppercase">{title}</h1>
+      <p className="paragraph text-[--black] opacity-75">{description}</p>
+    </div>
+  );
+};
