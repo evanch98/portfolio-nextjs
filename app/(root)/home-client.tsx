909553e358ccbdd9b6637615ece50ceaa08044f4
Kyaw Thu <evanch98@gmail.com>
2023-09-20 16:41:59 +0800

    update the HomeClient component

--- app/(root)/home-client.tsx
+++ app/(root)/home-client.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import Subheading from "@/components/common/subheading";
-import Banner from "@/components/common/banner";
-import ProjectsSection from "@/components/common/projects-section";
-import ToolsSection from "./components/tools-section";
-import ContactSection from "@/components/common/contact-section";
 import { MainNav } from "@/components/main-nav";
 import { Container } from "@/components/common/container";
 
 const HomeClient = () => {
   return (
-    <Container>
+    <div className="hero-background w-full h-full">
       <MainNav />
-    </Container>
+      <Container>
+        <div>Hello</div>
+      </Container>
+    </div>
   );
 };
 
