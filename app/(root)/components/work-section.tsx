f94bfc14c51d25a46778f19fe884e856a7ec81dd
Kyaw Thu <evanch98@gmail.com>
2023-09-20 18:05:50 +0800

    update the WorkSection.tsx

--- app/(root)/components/work-section.tsx
+++ app/(root)/components/work-section.tsx
@@ -2,8 +2,10 @@ import { Container } from "@/components/common/container";
 
 export const WorkSection = () => {
   return (
-    <Container>
-      <h1 className="heading-1 uppercase">My work</h1>
+    <Container className="pt-[256px] pb-[128px]">
+      <div className="flex flex-col items-center justify-center w-full gap-y-[128px]">
+        <h1 className="heading-1 uppercase text-[--black]">My work</h1>
+      </div>
     </Container>
   );
 };
