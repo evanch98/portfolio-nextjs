79ceeab2cf7c03313c5f88deec553ac9271981d7
Kyaw Thu <evanch98@gmail.com>
2023-09-20 18:13:02 +0800

    update the Header component

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -5,7 +5,7 @@ interface HeaderProps {
 
 export const Header = ({ title, description }: HeaderProps) => {
   return (
-    <div className="w-full flex flex-col items-center justify-center text-center gap-y-[24px]">
+    <div className="w-[790px] flex flex-col items-center justify-center text-center gap-y-[24px]">
       <h1 className="heading-1 text-[--black] uppercase">{title}</h1>
       <p className="paragraph text-[--black] opacity-75">{description}</p>
     </div>
