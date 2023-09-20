74465668cb37dc62764e03ddf2dbfc4286c1faa3
Kyaw Thu <evanch98@gmail.com>
2023-09-20 13:06:55 +0800

    update the HomeClient component

--- app/(root)/home-client.tsx
+++ app/(root)/home-client.tsx
@@ -6,9 +6,14 @@ import ProjectsSection from "@/components/common/projects-section";
 import ToolsSection from "./components/tools-section";
 import ContactSection from "@/components/common/contact-section";
 import { MainNav } from "@/components/main-nav";
+import { Container } from "@/components/common/container";
 
 const HomeClient = () => {
-  return <MainNav />;
+  return (
+    <Container>
+      <MainNav />
+    </Container>
+  );
 };
 
 export default HomeClient;
