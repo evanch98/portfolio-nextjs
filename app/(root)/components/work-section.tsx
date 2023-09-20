789ff20fd109308772e85b0a909809c5a324b64e
Kyaw Thu <evanch98@gmail.com>
2023-09-20 18:00:13 +0800

    add a new component WorkSection

---
+++ app/(root)/components/work-section.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@/components/common/container";
+
+export const WorkSection = () => {
+  return (
+    <Container>
+      <h1 className="heading-1 uppercase">My work</h1>
+    </Container>
+  );
+};
