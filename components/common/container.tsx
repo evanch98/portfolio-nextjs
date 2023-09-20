a285829d108ae39ace01d146b731467523be31d3
Kyaw Thu <evanch98@gmail.com>
2023-09-20 12:03:57 +0800

    update the Container component

--- components/common/container.tsx
+++ components/common/container.tsx
@@ -7,7 +7,12 @@ interface ContainerProps {
 
 export const Container = ({ className, children }: ContainerProps) => {
   return (
-    <div className={cn("max-w-[1200px] flex flex-col py-[256px]", className)}>
+    <div
+      className={cn(
+        "max-w-[1440px] flex flex-col py-[256px] px-[120px]",
+        className
+      )}
+    >
       {children}
     </div>
   );
