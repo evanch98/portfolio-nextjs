9d37e946775bb232742f86d89a9ee546f9cf326c
Kyaw Thu <evanch98@gmail.com>
2023-09-20 16:45:33 +0800

    update the HomeClient component

--- app/(root)/home-client.tsx
+++ app/(root)/home-client.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { MainNav } from "@/components/main-nav";
-import { Container } from "@/components/common/container";
 import { HeroSection } from "./components/hero-section";
 
 const HomeClient = () => {
