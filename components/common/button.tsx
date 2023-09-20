229d21a3f71905beea86fe5dce29b0cddc444848
Kyaw Thu <evanch98@gmail.com>
2023-09-20 17:35:31 +0800

    update the Button component

--- components/common/button.tsx
+++ components/common/button.tsx
@@ -6,10 +6,9 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const variantMap = {
-  hero: "border-[--primary-200] pl-[32px] pr-[24px] py-[16px] flex gap-x-[16px]",
+  hero: "border-[--primary-200] pl-[32px] pr-[24px] py-[16px] gap-x-[16px]",
   normal: "border-[--black] px-[32px] py-[16px]",
-  normalWithIcon:
-    "border-[--black] pl-[32px] pr-[24px] py-[16px] flex gap-x-[16px]",
+  normalWithIcon: "border-[--black] pl-[32px] pr-[24px] py-[16px] gap-x-[16px]",
 };
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(
@@ -18,7 +17,12 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(
     ref
   ) => {
     return (
-      <button className={cn("rounded-full button-text", variantMap[variant])}>
+      <button
+        className={cn(
+          "rounded-full button-text flex justify-center items-center",
+          variantMap[variant]
+        )}
+      >
         {children}
       </button>
     );
