1bfb0e79da121126791b950b9b998b29e3855230
Kyaw Thu <evanch98@gmail.com>
2023-09-21 20:14:08 +0800

    update the Footer component

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,5 +1,11 @@
+import { Container } from "./container";
+
 export const Footer = () => {
   return (
-    <div className="mt-[128px] border-t border-t-[--black] opacity-25"></div>
+    <div className="mt-[128px] w-full border-t border-t-[#c7c8c9]">
+      <Container className="py-[40px]">
+        <div className="flex items-center justify-between w-full"></div>
+      </Container>
+    </div>
   );
 };
