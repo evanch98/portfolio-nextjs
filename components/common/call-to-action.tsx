40f844ce0356f2e342b37c7582ba1732b60ee556
Kyaw Thu <evanch98@gmail.com>
2023-09-21 14:50:49 +0800

    update the CallToAction component

--- components/common/call-to-action.tsx
+++ components/common/call-to-action.tsx
@@ -1,3 +1,26 @@
+import { FaArrowRight } from "react-icons/fa6";
+import Button from "./button";
+import { Container } from "./container";
+
 export const CallToAction = () => {
-  return <div>Call To action</div>;
+  return (
+    <Container className="py-[128px]">
+      <div className="w-full px-[104px]">
+        <div className="flex flex-col gap-y-[24px] w-[690px]">
+          <h1 className="heading-2">Got a project in mind?</h1>
+          <div className="w-full flex flex-col gap-y-[40px]">
+            <p className="paragraph opacity-75">
+              If you're looking for a designer and developer who is passionate
+              about helping you business grow, let's talk.
+            </p>
+            <div>
+              <Button variant="normalWithIcon">
+                Let's Talk <FaArrowRight className="w-[20px] h-[20px]" />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
