00dfdd340aa41883dc1a0daaf11f5709d2b182fc
Kyaw Thu <evanch98@gmail.com>
2023-09-21 14:27:53 +0800

    update the ProjectCard component

--- app/(root)/components/project-card.tsx
+++ app/(root)/components/project-card.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@/components/common/container";
+import Button from "@/components/common/button";
 import Placeholder from "@/public/assets/placeholder.jpg";
 import Image from "next/image";
 
@@ -10,6 +10,13 @@ export const ProjectCard = () => {
       </div>
       <div className="flex flex-col items-center justify-center gap-y-[24px] w-[380px]">
         <h1 className="heading-3">Little Lemon Restaurant</h1>
+        <div className="w-full flex flex-col items-center justify-center gap-y-[32px]">
+          <p className="paragraph opacity-75">
+            Designing a streamlined restaurant website that helps customers
+            explore menu, order online, and reserve a table with ease.
+          </p>
+          <Button variant="normal">Read Case Study</Button>
+        </div>
       </div>
     </div>
   );
