358e1e18b6a57053dd8c0c9cb07e60c6f6493121
Kyaw Thu <evanch98@gmail.com>
2023-09-21 14:52:00 +0800

    update the CallToAction component

--- components/common/call-to-action.tsx
+++ components/common/call-to-action.tsx
@@ -8,7 +8,7 @@ export const CallToAction = () => {
       <div className="w-full px-[104px]">
         <div className="flex flex-col gap-y-[24px] w-[690px]">
           <h1 className="heading-2">Got a project in mind?</h1>
-          <div className="w-full flex flex-col gap-y-[40px]">
+          <div className="w-full flex flex-col gap-y-[48px]">
             <p className="paragraph opacity-75">
               If you're looking for a designer and developer who is passionate
               about helping you business grow, let's talk.
