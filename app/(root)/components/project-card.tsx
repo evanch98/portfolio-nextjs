3e6c64023b7f6c60f3671468b56398d096994f66
Kyaw Thu <evanch98@gmail.com>
2023-09-21 14:03:07 +0800

    add a new component ProjectCard

--- app/(root)/components/project-card.tsx
+++ app/(root)/components/project-card.tsx
@@ -1,3 +1,12 @@
+import { Container } from "@/components/common/container";
+import Placeholder from "@/public/assets/placeholder.jpg";
+import Image from "next/image";
+
 export const ProjectCard = () => {
-  return <div>Project Card</div>;
+  return (
+    <Container>
+      <div className="flex items-center justify-between"></div>
+      <Image src={Placeholder} width={690} height={500} alt="Image" />
+    </Container>
+  );
 };
