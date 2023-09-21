0ad55326a044c5554a35144d1c7e701c288eb429
Kyaw Thu <evanch98@gmail.com>
2023-09-21 14:23:34 +0800

    update the globals.css

--- app/(root)/components/project-card.tsx
+++ app/(root)/components/project-card.tsx
@@ -4,10 +4,13 @@ import Image from "next/image";
 
 export const ProjectCard = () => {
   return (
-    <Container>
-      <div className="flex items-center justify-between">
-        <div className="w-[690px] h-[500px]"></div>
+    <div className="flex items-center justify-between w-full">
+      <div className="w-[690px] h-[500px] relative">
+        <Image src={Placeholder} layout="fill" objectFit="cover" alt="Image" />
       </div>
-    </Container>
+      <div className="flex flex-col items-center justify-center gap-y-[24px] w-[380px]">
+        <h1 className="heading-3">Little Lemon Restaurant</h1>
+      </div>
+    </div>
   );
 };
