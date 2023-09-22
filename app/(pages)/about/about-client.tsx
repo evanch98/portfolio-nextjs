83ad1d89317827ad727cec99999530ec1c0d59dd
Kyaw Thu <evanch98@gmail.com>
2023-09-22 20:01:49 +0800

    add the TechnicalStack

--- app/(pages)/about/about-client.tsx
+++ app/(pages)/about/about-client.tsx
@@ -3,12 +3,14 @@
 import { CallToAction } from "@/components/common/call-to-action";
 import IntroSection from "./components/intro-section";
 import { MyValues } from "./components/my-values";
+import { TechnicalStack } from "./components/technical-stack";
 
 const AboutClient = () => {
   return (
     <>
       <IntroSection />
       <MyValues />
+      <TechnicalStack />
       <CallToAction />
     </>
   );
