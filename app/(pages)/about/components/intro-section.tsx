d9fb9251b6f5610f770478b5972473fece3b3e00
Kyaw Thu <evanch98@gmail.com>
2023-09-22 12:39:44 +0800

    update the IntroSection component

--- app/(pages)/about/components/intro-section.tsx
+++ app/(pages)/about/components/intro-section.tsx
@@ -1,11 +1,29 @@
 "use client";
 
 import { Container } from "@/components/common/container";
+import Link from "next/link";
 
 const IntroSection = () => {
   return (
-    <Container>
-      <div>Intro</div>
+    <Container className="pt-[216px] pb-[128px] text-[--black]">
+      <div className="px-[104px] w-full">
+        <div className="flex flex-col gap-y-[24px] w-[590px]">
+          <h1 className="heading-3">Hey, nice to meet you!</h1>
+          <p className="paragraph opacity-75">
+            I’m Kyaw Thu. I,m a UI/UX designer and front-end developer who likes
+            to build beautiful and modern-looking websites using the latest
+            technologies. Scroll down to learn about me,{" "}
+            <span className="underline hover:no-underline ease-in duration-300 hover:opacity-50">
+              <Link href="/#my-work">check out my work</Link>
+            </span>{" "}
+            or{" "}
+            <span className="underline hover:no-underline ease-in duration-300 hover:opacity-50">
+              <Link href="/contact">drop me a line</Link>
+            </span>
+            .
+          </p>
+        </div>
+      </div>
     </Container>
   );
 };
