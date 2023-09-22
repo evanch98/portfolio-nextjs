cd05f7c0d53fec3a80799ec7c32ebb2d1d6e4ee3
Kyaw Thu <evanch98@gmail.com>
2023-09-22 20:24:16 +0800

    add AboutMe

--- app/(pages)/about/about-client.tsx
+++ app/(pages)/about/about-client.tsx
@@ -4,6 +4,7 @@ import { CallToAction } from "@/components/common/call-to-action";
 import IntroSection from "./components/intro-section";
 import { MyValues } from "./components/my-values";
 import { TechnicalStack } from "./components/technical-stack";
+import { AboutMe } from "./components/about-me";
 
 const AboutClient = () => {
   return (
@@ -11,6 +12,7 @@ const AboutClient = () => {
       <IntroSection />
       <MyValues />
       <TechnicalStack />
+      <AboutMe />
       <CallToAction />
     </>
   );
