90c436cfa10fc0fb7a8d84eeb88249c24833dc4c
Kyaw Thu <evanch98@gmail.com>
2023-09-22 19:37:02 +0800

    update MyValues

--- app/(pages)/about/components/my-values.tsx
+++ app/(pages)/about/components/my-values.tsx
@@ -1,3 +1,13 @@
+import { Container } from "@/components/common/container";
+import { Header } from "@/components/common/header";
+
 export const MyValues = () => {
-  return <div>My Values</div>;
+  return (
+    <Container className="py-[128px]">
+      <Header
+        title="My Values"
+        description="I like to promise only what I can deliver and then deliver more than I promise."
+      />
+    </Container>
+  );
 };
