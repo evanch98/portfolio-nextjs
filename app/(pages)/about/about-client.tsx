b8617aa980e580ebb5702d87c6786b286ee0264a
Kyaw Thu <evanch98@gmail.com>
2023-09-22 12:26:58 +0800

    update the AboutClient component

--- app/(pages)/about/about-client.tsx
+++ app/(pages)/about/about-client.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { CallToAction } from "@/components/common/call-to-action";
 import IntroSection from "./components/intro-section";
 
 const AboutClient = () => {
   return (
     <>
       <IntroSection />
+      <CallToAction />
     </>
   );
 };
