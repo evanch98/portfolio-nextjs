336a25786a3011a31b70d67811a725cb96892f5c
Kyaw Thu <evanch98@gmail.com>
2023-09-22 19:42:35 +0800

    update the MyValues component

--- app/(pages)/about/components/my-values.tsx
+++ app/(pages)/about/components/my-values.tsx
@@ -4,10 +4,16 @@ import { Header } from "@/components/common/header";
 export const MyValues = () => {
   return (
     <Container className="py-[128px]">
-      <Header
-        title="My Values"
-        description="I like to promise only what I can deliver and then deliver more than I promise."
-      />
+      <div className="w-full flex flex-col items-center justify-center gap-y-[128px]">
+        <Header
+          title="My Values"
+          description="I like to promise only what I can deliver and then deliver more than I promise."
+        />
+        <div className="flex flex-col gap-y-[96px] w-[790px]">
+          <div className="flex items-center w-full gap-x-[64px]"></div>
+          <div className="flex items-center w-full gap-x-[64px]"></div>
+        </div>
+      </div>
     </Container>
   );
 };
