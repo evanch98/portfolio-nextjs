7c60ba55cf8f2ed8e89afba583603290597b8311
Kyaw Thu <evanch98@gmail.com>
2023-09-22 11:48:01 +0800

    update the Capabilities component

--- app/(root)/components/capabilities.tsx
+++ app/(root)/components/capabilities.tsx
@@ -1,9 +1,15 @@
 import { Container } from "@/components/common/container";
+import { Header } from "@/components/common/header";
 
 export const Capabilities = () => {
   return (
     <Container className="py-[128px]">
-      <div>Capabilities</div>
+      <div className="flex flex-col gap-y-[128px] items-center justify-center">
+        <Header
+          title="Capabilities"
+          description="Transforming complex ideas into intuitive and human-centered experiences"
+        />
+      </div>
     </Container>
   );
 };
