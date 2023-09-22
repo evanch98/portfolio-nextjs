cdbf038ff22d4aa9da078483ed6569cfeaecce3a
Kyaw Thu <evanch98@gmail.com>
2023-09-22 12:26:16 +0800

    update the AboutClient component

--- app/(pages)/about/about-client.tsx
+++ app/(pages)/about/about-client.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import Banner from "@/components/common/banner";
-import Subheading from "@/components/common/subheading";
 import IntroSection from "./components/intro-section";
-import ContactSection from "@/components/common/contact-section";
 
 const AboutClient = () => {
   return (
     <>
-      <div>About</div>
+      <IntroSection />
     </>
   );
 };
