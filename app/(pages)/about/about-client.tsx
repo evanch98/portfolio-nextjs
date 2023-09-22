d32e9af9a5c642969588cad08c99d88d2d948989
Kyaw Thu <evanch98@gmail.com>
2023-09-22 12:25:02 +0800

    update the AboutClient component

--- app/(pages)/about/about-client.tsx
+++ app/(pages)/about/about-client.tsx
@@ -7,22 +7,9 @@ import ContactSection from "@/components/common/contact-section";
 
 const AboutClient = () => {
   return (
-    <div className="w-full h-full mt-[70px] mb-5 flex items-center justify-center">
-      <div className="h-full w-full max-w-[1024px] flex flex-col items-center justify-center px-2 lg:px-0 gap-5">
-        <Banner
-          heading="About Me"
-          description="An aspiring front-end developer"
-        />
-        <section className="w-full flex flex-col gap-5">
-          <Subheading title="Introduction" />
-          <IntroSection />
-        </section>
-        <section className="w-full flex flex-col gap-5">
-          <Subheading title="Contact Me" />
-          <ContactSection />
-        </section>
-      </div>
-    </div>
+    <>
+      <div>About</div>
+    </>
   );
 };
 
