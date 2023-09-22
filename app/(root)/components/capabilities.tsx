0e01891a7d7b58fc812edffd669668c50bc96bf4
Kyaw Thu <evanch98@gmail.com>
2023-09-22 11:44:46 +0800

    add a new component Capabilities

---
+++ app/(root)/components/capabilities.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@/components/common/container";
+
+export const Capabilities = () => {
+  return (
+    <Container className="py-[128px]">
+      <div>Capabilities</div>
+    </Container>
+  );
+};
