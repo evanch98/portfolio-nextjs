96e5d3d68de8813bd36bad32409e49d4cc9803e3
Kyaw Thu <evanch98@gmail.com>
2023-09-22 12:05:36 +0800

    update the SkillBox component

--- components/skill-box.tsx
+++ components/skill-box.tsx
@@ -1,3 +1,25 @@
-export const SkillBox = () => {
-  return <div>SkillBox</div>;
+import { cn } from "@/lib/utils";
+
+interface SkillBoxProps {
+  border?: "top" | "bottom" | "topAndBottom";
+  name: string;
+}
+
+const borderMap = {
+  top: "border-t border-t-[#c7c8c9]",
+  bottom: "border-b border-b-[#c7c8c9]",
+  topAndBottom: "border-t border-b border-t-[#c7c8c9] border-b-[#c7c8c9]",
+};
+
+export const SkillBox = ({ border = "top", name }: SkillBoxProps) => {
+  return (
+    <div
+      className={cn(
+        "flex flex-col py-[8px] px-[16px] w-full items-center justify-center",
+        borderMap[border]
+      )}
+    >
+      <p className="heading-6">{name}</p>
+    </div>
+  );
 };
