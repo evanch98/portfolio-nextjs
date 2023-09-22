ba812ff459b1fb9b9b654f13bd497514111387b3
Kyaw Thu <evanch98@gmail.com>
2023-09-22 11:58:55 +0800

    update the Capabilities component

--- app/(root)/components/capabilities.tsx
+++ app/(root)/components/capabilities.tsx
@@ -1,5 +1,6 @@
 import { Container } from "@/components/common/container";
 import { Header } from "@/components/common/header";
+import { SkillBox } from "@/components/skill-box";
 
 export const Capabilities = () => {
   return (
@@ -9,6 +10,16 @@ export const Capabilities = () => {
           title="Capabilities"
           description="Transforming complex ideas into intuitive and human-centered experiences"
         />
+        <div className="flex items-center justify-center gap-x-[24px]">
+          <section className="flex flex-col items-center justify-center gap-y-[24px]">
+            <h2 className="heading-3">Skills</h2>
+            <SkillBox />
+          </section>
+          <section className="flex flex-col items-center justify-center gap-y-[24px]">
+            <h2 className="heading-3">Tools</h2>
+            <SkillBox />
+          </section>
+        </div>
       </div>
     </Container>
   );
