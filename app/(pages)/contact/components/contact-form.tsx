4731b66b3534a8970e866cae0fffaa012d8243f3
Kyaw Thu <evanch98@gmail.com>
2023-09-23 11:57:58 +0800

    add a new component ContactForm

---
+++ app/(pages)/contact/components/contact-form.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@/components/common/container";
+
+export const ContactForm = () => {
+  return (
+    <Container className="py-[128px]">
+      <div className="w-full px-[104px] text-[--black]">Form</div>
+    </Container>
+  );
+};
