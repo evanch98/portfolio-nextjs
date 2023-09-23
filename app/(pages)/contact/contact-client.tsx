207a10770e2b4f926f170809e287f481bb77d2c1
Kyaw Thu <evanch98@gmail.com>
2023-09-23 11:58:27 +0800

    add the ContactForm

--- app/(pages)/contact/contact-client.tsx
+++ app/(pages)/contact/contact-client.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { ContactForm } from "./components/contact-form";
 import IntroSection from "./components/intro-section";
 
 export const ContactClient = () => {
   return (
     <>
       <IntroSection />
+      <ContactForm />
     </>
   );
 };
