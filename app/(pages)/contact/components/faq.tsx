81566bfd7a7f07bd4a2dd1c42061e4c833736829
Kyaw Thu <evanch98@gmail.com>
2023-09-23 15:53:52 +0800

    add a new component Faq

---
+++ app/(pages)/contact/components/faq.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@/components/common/container";
+
+const Faq = () => {
+  return (
+    <Container className="py-[128px]">
+      <div className="w-full px-[104px]"></div>
+    </Container>
+  );
+};
+
+export default Faq;
