76b9eca68da10ea884ea24bda0883965d8fab201
Kyaw Thu <evanch98@gmail.com>
2023-09-23 17:13:42 +0800

    update the IntroSection component

--- app/(pages)/contact/components/intro-section.tsx
+++ app/(pages)/contact/components/intro-section.tsx
@@ -1,20 +1,38 @@
 "use client";
 
 import { Container } from "@/components/common/container";
+import { motion } from "framer-motion";
+
+const titleVariant = {
+  hidden: { y: -50 },
+  visible: { y: 0, transition: { duration: 0.5, type: "ease-in" } },
+};
+
+const descriptionVariant = {
+  hidden: { y: 50 },
+  visible: { y: 0, transition: { duration: 0.5, type: "ease-in" } },
+};
 
 const IntroSection = () => {
   return (
     <Container className="pt-[216px] pb-[128px] text-[--black]">
       <div className="px-[104px] w-full">
-        <div className="flex flex-col gap-y-[24px] w-[590px]">
-          <h1 className="heading-3">
+        <motion.div
+          className="flex flex-col gap-y-[24px] w-[590px]"
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.h1 variants={titleVariant} className="heading-3">
             Want to discuss a project or opportunity?
-          </h1>
-          <p className="paragraph opacity-75">
+          </motion.h1>
+          <motion.p
+            variants={descriptionVariant}
+            className="paragraph opacity-75"
+          >
             Use the form below to give me some details, and I'll get back to you
             as soon as I can.
-          </p>
-        </div>
+          </motion.p>
+        </motion.div>
       </div>
     </Container>
   );
