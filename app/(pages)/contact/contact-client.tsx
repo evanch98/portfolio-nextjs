19d95573de6be741f6f139c58accd1207cea5bc1
Kyaw Thu <evanch98@gmail.com>
2023-09-23 11:55:38 +0800

    add the IntroSection

--- app/(pages)/contact/contact-client.tsx
+++ app/(pages)/contact/contact-client.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import IntroSection from "./components/intro-section";
+
 export const ContactClient = () => {
-  return <></>;
+  return (
+    <>
+      <IntroSection />
+    </>
+  );
 };
