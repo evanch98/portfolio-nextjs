e253c0a67f51a589e9eb23f3d1a1529694a51e10
Kyaw Thu <evanch98@gmail.com>
2023-09-24 14:15:25 +0800

    update the metadata

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import AboutClient from "./about-client";
+
+export const metadata: Metadata = {
+  title: "Kyaw | About Me",
+  description: "Kyaw Thu's portfolio website about me page",
+};
 
 export default function AboutPage() {
   return (
--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import { ContactClient } from "./contact-client";
+
+export const metadata: Metadata = {
+  title: "Kyaw | Contact Me",
+  description: "Kyaw Thu's portfolio website contact me page",
+};
 
 export default function ContactPage() {
   return (
