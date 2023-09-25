c943e63d5a8d68cf195ab7dac09d54649bd6a541
Kyaw Thu <evanch98@gmail.com>
2023-09-25 11:50:17 +0800

    update the utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const hVariant = {
+  hidden: { opacity: 0, y: -50 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.7, ease: "easeIn" } },
+};
+
+export const pVariant = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.7, ease: "easeIn" } },
+};
