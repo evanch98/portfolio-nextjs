bb58397e2d3ff5cc0487968492a5714b5de5f48d
Kyaw Thu <evanch98@gmail.com>
2023-09-25 12:00:23 +0800

    update the animation

--- app/(root)/components/project-card.tsx
+++ app/(root)/components/project-card.tsx
@@ -1,7 +1,13 @@
 import Button from "@/components/common/button";
-import { cn } from "@/lib/utils";
+import { cn, hVariant, pVariant } from "@/lib/utils";
 import Placeholder from "@/public/assets/placeholder.jpg";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
+const imgVariant = {
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { duration: 0.7, ease: "easeIn" } },
+};
 
 interface ProjectCardProps {
   imageSide?: "left" | "right";
@@ -9,20 +15,36 @@ interface ProjectCardProps {
 
 export const ProjectCard = ({ imageSide = "left" }: ProjectCardProps) => {
   return (
-    <div className={cn("flex items-center justify-between w-full", imageSide === "right" ? "flex-row-reverse" : "")}>
-      <div className="w-[690px] h-[500px] relative">
+    <motion.div
+      className={cn(
+        "flex items-center justify-between w-full",
+        imageSide === "right" ? "flex-row-reverse" : ""
+      )}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true }}
+    >
+      <motion.div
+        variants={imgVariant}
+        className="w-[690px] h-[500px] relative"
+      >
         <Image src={Placeholder} layout="fill" objectFit="cover" alt="Image" />
-      </div>
+      </motion.div>
       <div className="flex flex-col items-center justify-center gap-y-[24px] w-[380px]">
-        <h1 className="heading-3">Little Lemon Restaurant</h1>
-        <div className="w-full flex flex-col items-center justify-center gap-y-[48px]">
+        <motion.h1 variants={hVariant} className="heading-3">
+          Little Lemon Restaurant
+        </motion.h1>
+        <motion.div
+          variants={pVariant}
+          className="w-full flex flex-col items-center justify-center gap-y-[48px]"
+        >
           <p className="paragraph opacity-75">
             Designing a streamlined restaurant website that helps customers
             explore menu, order online, and reserve a table with ease.
           </p>
           <Button variant="normal">Read Case Study</Button>
-        </div>
+        </motion.div>
       </div>
-    </div>
+    </motion.div>
   );
 };
