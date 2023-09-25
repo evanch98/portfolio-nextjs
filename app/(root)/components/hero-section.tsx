5f956ddd99b2f7688b0b548bcdfae9f434cdbe87
Kyaw Thu <evanch98@gmail.com>
2023-09-25 11:45:50 +0800

    update the HeroSection

--- app/(root)/components/hero-section.tsx
+++ app/(root)/components/hero-section.tsx
@@ -1,30 +1,49 @@
 import Button from "@/components/common/button";
 import { Container } from "@/components/common/container";
+import { motion } from "framer-motion";
 import { FaArrowRight } from "react-icons/fa6";
 import Link from "next/link";
+
+const hVariant = {
+  hidden: { opacity: 0, y: -50 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.7, ease: "easeIn" } },
+};
+
+const pVariant = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0, transition: { duration: 0.7, ease: "easeIn" } },
+};
 
 export const HeroSection = () => {
   return (
     <div className="hero-background w-full h-full">
       <Container className="py-[128px]">
-        <div className="flex flex-col items-center justify-center gap-y-[32px] text-[--primary-100]">
-          <h2 className="heading-3">Hey there, I'm Kyaw Thu</h2>
-          <h1 className="heading-2 text-center">
+        <motion.div
+          className="flex flex-col items-center justify-center gap-y-[32px] text-[--primary-100]"
+          initial="hidden"
+          animate="visible"
+        >
+          <motion.h2 variants={hVariant} className="heading-3">
+            Hey there, I'm Kyaw Thu
+          </motion.h2>
+          <motion.h1 variants={hVariant} className="heading-2 text-center">
             Freelance UX/UI designer & web developer based in Taiwan
-          </h1>
+          </motion.h1>
           <div className="flex flex-col items-center justify-center gap-y-[48px] text-[--primary-200]">
-            <p className="paragraph text-center">
+            <motion.p variants={pVariant} className="paragraph text-center">
               A designer/front-end developer with a passion for helping startups
               through unique and user-centered design.
-            </p>
+            </motion.p>
             <Link href="/#my-work">
-              <Button variant="hero" className="group">
-                View my work{" "}
-                <FaArrowRight className="w-[20px] h-[20px] group-hover:rotate-90 ease-in duration-300" />
-              </Button>
+              <motion.div variants={pVariant}>
+                <Button variant="hero" className="group">
+                  View my work{" "}
+                  <FaArrowRight className="w-[20px] h-[20px] group-hover:rotate-90 ease-in duration-300" />
+                </Button>
+              </motion.div>
             </Link>
           </div>
-        </div>
+        </motion.div>
       </Container>
     </div>
   );
