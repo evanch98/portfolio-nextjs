fecef05f6a14b8dc6165a7dfde5fa8cda598719c
Kyaw Thu <evanch98@gmail.com>
2023-09-25 11:49:07 +0800

    update the Header component

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { motion } from "framer-motion";
+
 interface HeaderProps {
   title: string;
   description: string;
@@ -5,9 +9,13 @@ interface HeaderProps {
 
 export const Header = ({ title, description }: HeaderProps) => {
   return (
-    <div className="w-[790px] flex flex-col items-center justify-center text-center gap-y-[24px]">
+    <motion.div
+      className="w-[790px] flex flex-col items-center justify-center text-center gap-y-[24px]"
+      initial="hidden"
+      animate="visible"
+    >
       <h1 className="heading-1 text-[--black] uppercase">{title}</h1>
       <p className="paragraph text-[--black] opacity-75">{description}</p>
-    </div>
+    </motion.div>
   );
 };
