8e2d7ab546f68628300c0a721a1742b7ebfe56ef
Kyaw Thu <evanch98@gmail.com>
2023-10-15 19:39:33 +0800

    update the WorkSection

--- app/(root)/components/work-section.tsx
+++ app/(root)/components/work-section.tsx
@@ -1,8 +1,26 @@
 import { Container } from "@/components/common/container";
 import { Header } from "@/components/common/header";
 import { ProjectCard } from "./project-card";
+import { useState } from "react";
+import { Popover } from "@/components/ui/popover";
+import { PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
+import { ChevronsUpDown } from "lucide-react";
+
+const categories = [
+  {
+    value: "webApp",
+    label: "Web Applications",
+  },
+  {
+    value: "uiux",
+    label: "UI/UX",
+  },
+];
 
 export const WorkSection = () => {
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("webApp");
+
   return (
     <Container className="md:pt-[256px] pt-[64px] md:pb-[128px] pb-[32px] text-[--black]">
       <div
@@ -13,12 +31,23 @@ export const WorkSection = () => {
           title="My work"
           description="I specialize in designing and building modern-looking and user-centered websites."
         />
-        {/* TODO: Update the ProjectCard and projects */}
-        <div className="w-full flex flex-col items-center justify-center md:space-y-[96px] space-y-[56px]">
-          <ProjectCard />
-          <ProjectCard imageSide="right" />
-          <ProjectCard />
-          <ProjectCard imageSide="right" />
+        <div className="w-full md:space-y-[48px] space-y-[32px]">
+          <Popover open={open} onOpenChange={setOpen}>
+            <PopoverTrigger asChild>
+              <button className="flex items-center justify-center gap-x-[16px] py-[16px] px-[24px] border border-[--black] rounded-full">
+                {categories.find((category) => category.value === value)?.label}
+                <ChevronsUpDown className="ml-2 h-[20px] w-[20px] shrink-0" />
+              </button>
+            </PopoverTrigger>
+            <PopoverContent className="w-[200px] p-0"></PopoverContent>
+          </Popover>
+          {/* TODO: Update the ProjectCard and projects */}
+          <div className="w-full flex flex-col items-center justify-center md:space-y-[96px] space-y-[56px]">
+            <ProjectCard />
+            <ProjectCard imageSide="right" />
+            <ProjectCard />
+            <ProjectCard imageSide="right" />
+          </div>
         </div>
       </div>
     </Container>
