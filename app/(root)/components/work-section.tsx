d38cf5ab1bd0f225135792e7e4139615646afd35
Kyaw Thu <evanch98@gmail.com>
2023-10-15 19:55:26 +0800

    update the WorkSection

--- app/(root)/components/work-section.tsx
+++ app/(root)/components/work-section.tsx
@@ -2,17 +2,22 @@ import { Container } from "@/components/common/container";
 import { Header } from "@/components/common/header";
 import { ProjectCard } from "./project-card";
 import { useState } from "react";
-import { Popover } from "@/components/ui/popover";
-import { PopoverContent, PopoverTrigger } from "@radix-ui/react-popover";
-import { ChevronsUpDown } from "lucide-react";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { Command, CommandGroup, CommandItem } from "@/components/ui/command";
+import { cn } from "@/lib/utils";
 
 const categories = [
   {
-    value: "webApp",
+    value: "web applications",
     label: "Web Applications",
   },
   {
-    value: "uiux",
+    value: "ui/ux",
     label: "UI/UX",
   },
 ];
@@ -34,12 +39,40 @@ export const WorkSection = () => {
         <div className="w-full md:space-y-[48px] space-y-[32px]">
           <Popover open={open} onOpenChange={setOpen}>
             <PopoverTrigger asChild>
-              <button className="flex items-center justify-center gap-x-[16px] py-[16px] px-[24px] border border-[--black] rounded-full">
+              <button
+                role="combobox"
+                aria-aria-expanded={open}
+                className="flex items-center justify-center gap-x-[16px] py-[16px] px-[24px] border border-[--black] rounded-full"
+              >
                 {categories.find((category) => category.value === value)?.label}
                 <ChevronsUpDown className="ml-2 h-[20px] w-[20px] shrink-0" />
               </button>
             </PopoverTrigger>
-            <PopoverContent className="w-[200px] p-0"></PopoverContent>
+            <PopoverContent className="w-[200px] p-0">
+              <Command>
+                <CommandGroup>
+                  {categories.map((category) => (
+                    <CommandItem
+                      key={category.value}
+                      onSelect={(currentValue) => {
+                        console.log(currentValue);
+
+                        setValue(currentValue);
+                        setOpen(false);
+                      }}
+                    >
+                      <Check
+                        className={cn(
+                          "mr-2 h-4 w-4",
+                          value === category.value ? "opacity-100" : "opacity-0"
+                        )}
+                      />
+                      {category.label}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              </Command>
+            </PopoverContent>
           </Popover>
           {/* TODO: Update the ProjectCard and projects */}
           <div className="w-full flex flex-col items-center justify-center md:space-y-[96px] space-y-[56px]">
