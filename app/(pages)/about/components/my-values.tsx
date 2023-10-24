157a6f1a3a145686df0ead02a9bb3a78a7b1d2cb
Kyaw Thu <evanch98@gmail.com>
2023-10-24 15:57:15 +0800

    update the MyValues component

--- app/(pages)/about/components/my-values.tsx
+++ app/(pages)/about/components/my-values.tsx
@@ -4,14 +4,14 @@ import { ValueBox } from "@/components/value-box";
 
 export const MyValues = () => {
   return (
-    <Container className="py-[128px]">
-      <div className="w-full flex flex-col items-center justify-center gap-y-[128px] text-[--black]">
+    <Container className="py-[64px] md:py-[128px]">
+      <div className="w-full flex flex-col items-center justify-center gap-y-[64px] md:gap-y-[128px] text-[--black]">
         <Header
           title="My Values"
           description="I like to promise only what I can deliver and then deliver more than I promise."
         />
-        <div className="flex flex-col gap-y-[96px] w-[790px]">
-          <div className="flex items-center w-full gap-x-[64px]">
+        <div className="flex flex-col gap-y-[32px] md:gap-y-[96px] md:w-[790px]">
+          <div className="flex flex-col md:flex-row items-center w-full gap-y-[32px] md:gap-x-[64px]">
             <ValueBox
               title="On-time delivery"
               description="I understand the value of your time, and I want to assure you that I am committed to delivering exactly what you've paid for, as promised."
@@ -21,7 +21,7 @@ export const MyValues = () => {
               description="My top priority is always ensuring quality, as I strive to deliver a final result that you will genuinely appreciate."
             />
           </div>
-          <div className="flex items-center w-full gap-x-[64px]">
+          <div className="flex flex-col md:flex-row items-center w-full gap-y-[32px] md:gap-x-[64px]">
             <ValueBox
               title="Humility"
               description="I am grateful for the opportunities I've been given and humbled by the support I've received along the way."
